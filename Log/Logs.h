c09121d32b974b4531d667e258b218f1603a914f
sk153407 <sk153407@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-10-28 09:29:23 +0000

    *** empty log message ***

--- Log/Logs.h
+++ Log/Logs.h
@@ -1,31 +1,53 @@
+#ifndef __LOGS_H__
+#define __LOGS_H__
+
+#include <string>
 #include <stdio.h>
+#include "../Store/Store.h"
 
-class LogManager
+using namespace Store;
+
+namespace Logs
 {
-  /**
-   * Zapisuje do dziennika <BEGIN tid>.
-   */
-  void beginTransaction( TransactionID tid ) { printf( "LogManager: beginTransaction\n"); }
 
-  /**
-   * Nakazuje zapisac w logach informacje o starej i nowej wartosci
-   * zmodyfikowanego atrybutu.
-   */
-  void write( TransactionID tid, LogicalID lid, Value oldVal, Value newVal ) { printf( "LogManager: write\n" ); }
+  class LogManager
+  {
+    public:
 
-  /**
-   * Rozpoczyna tworzenie bezkonfliktowego punktu kontrolnego.
-   */
-  void checkpoint( TransactionID tid ) { printf( "LogManager: checkpoint\n"); }
+    LogManager() {}
+    
+    int init( string logPath ) { printf( "LogManager: initialize( logPath=%s )\n", logPath.c_str() ); return 0; }
+    
+    /**
+     * Zapisuje do dziennika <BEGIN tid>.
+     */
+    int beginTransaction( TransactionID tid ) { printf( "LogManager: beginTransaction\n"); return 0; }
 
-  /**
-   * Konczy sie w chwili gdy wszystkie logi transakcji tid znajda sie na dysku;
-   * wtedy dopiero mozna zaczac zrzucac brudnych stron do bazy na dysku.
-   */
-  void commitTransaction( TransactionID tid ) { printf( "LogManager: commitTransaction\n" ); }
+    /**
+     * Nakazuje zapisac w logach informacje o starej i nowej wartosci
+     * zmodyfikowanego atrybutu.
+     */
+    int write( TransactionID tid, LogicalID lid, DataValue oldVal, DataValue newVal ) { printf( "LogManager: write\n" ); return 0; }
 
-  /**
-   * Zapisuje do dziennika <ABORT tid>.
-   */
-  void rollbackTransaction( TransactionID tid ) { printf( "LogManager: rollbackTransaction\n" ); }
+    /**
+     * Rozpoczyna tworzenie bezkonfliktowego punktu kontrolnego.
+     */
+    int checkpoint( TransactionID tid ) { printf( "LogManager: checkpoint\n"); return 0; }
+
+    /**
+     * Konczy sie w chwili gdy wszystkie logi transakcji tid znajda sie na dysku;
+     * wtedy dopiero mozna zaczac zrzucac brudnych stron do bazy na dysku.
+     */
+    int commitTransaction( TransactionID tid ) { printf( "LogManager: commitTransaction\n" ); return 0; }
+
+    /**
+     * Zapisuje do dziennika <ABORT tid>.
+     */
+    int rollbackTransaction( TransactionID tid ) { printf( "LogManager: rollbackTransaction\n" ); return 0; }
+
+    int destroy() { printf( "LogManager: destroy()\n" ); return 0; }
+  };
+
 };
+
+#endif
