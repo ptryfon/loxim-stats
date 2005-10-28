c9939f41797922a83cd2142e8edc2f2cfbca4713
mh209206 <mh209206@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-10-28 09:45:22 +0000

    *** empty log message ***

--- QueryParser/QueryParser.cpp
+++ QueryParser/QueryParser.cpp
@@ -1,15 +1,15 @@
 #include<string>
 #include<iostream>
 #include<stdio.h>
-#include "QueryTree.h"
+#include "QueryParser.h"
 
 using namespace std;
 
-
+/*
 class Parser {
 
     protected:
-    int mojeImie;
+    int myNr;
     public:
     Parser() {};
     virtual ~Parser(){};
@@ -22,16 +22,22 @@ class Parser {
     //virtual int funkcja_a (int a) = 0;
 
 };
+*/
 
+    int QueryParser::parseIt (string s, QueryTree &qTrees) {
+	printf("Ja, PARSER, wczytalem takie cos...:\n\n %s \n\n",s.c_str());
+//	printf("Jestem Parser i parsuje %s stringa!\n", s);
+	return 0;    
 
+    };
 
 
 int main() {
 
-    Parser prsr = Parser();
+    QueryParser prsr = QueryParser();
     QueryTree *costam = new QueryTree[10];
-//    cout<<"zaraz sparsuje stringa\n";
-    prsr.parseIt("JestemZapytanieStringowe", costam);
+    cout<<"zaraz sparsuje stringa\n";
+//    prsr.parseIt("JestemZapytanieStringowe", costam);
 //    cout<<"juz cos zrobilem\n";
     delete [] costam;
     // jakis wpis do wcommitowania js209298    
