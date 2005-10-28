f69c8fd94c1c688d21f32a3ec3b4eff1d81ed03e
mh209206 <mh209206@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-10-28 09:54:10 +0000

    *** empty log message ***

---
+++ QueryParser/QueryParser.h
@@ -0,0 +1,38 @@
+#ifndef _QPARSER
+#define _QPARSER
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class QueryTree
+{
+
+private:
+    int nodesNum;
+    QueryTree *lSon;
+    QueryTree *rSon;
+    
+public:
+    QueryTree() {};
+    virtual ~QueryTree(){};
+    int toString(){
+	return 0;
+    };
+
+};
+
+class QueryParser 
+{
+
+    protected:
+    string myName;
+    public:
+    QueryParser() {};
+    virtual ~QueryParser() {};
+    int parseIt (string s, QueryTree &qTrees);
+
+};
+
+#endif
