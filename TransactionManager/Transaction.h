b6971c1c69b6c17ec748c5d4c821a0fed937b889
baggins <baggins@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-03 13:54:22 +0000

    - tymczasowo cofniete wciecie zeby nie komplikowac commitow

--- TransactionManager/Transaction.h
+++ TransactionManager/Transaction.h
@@ -12,55 +12,55 @@ using namespace Store;
 
 namespace TManager
 {
-	/**
-	 *	TransactionID - obiekt jednoznacznie identyfikujacy transakcje
-	 */
-	class TransactionID 
-	{      
-	      public:
-	      TransactionID(int n);
-	};
+/**
+ *	TransactionID - obiekt jednoznacznie identyfikujacy transakcje
+ */
+class TransactionID 
+{      
+      public:
+      TransactionID(int n);
+};
 
-	/**
-	 *   Transaction - obiekt w ramach ktorego dziala Executor
-	 */
-	class Transaction
-	{
-	    friend class TransactionManager;
-	    TransactionID *tid;
-	    StoreManager* sm;
+/**
+ *   Transaction - obiekt w ramach ktorego dziala Executor
+ */
+class Transaction
+{
+    friend class TransactionManager;
+    TransactionID *tid;
+    StoreManager* sm;
 
-	      public:
-		    Transaction();
-		    Transaction(TransactionID* tId);
-		    int getObjectPointer(LogicalID* lid, AccessMode mode, ObjectPointer* &p);
-	      private:
-		    void setSM(StoreManager*);
-		    void setID(int);
-	}; 
+      public:
+	    Transaction();
+	    Transaction(TransactionID* tId);
+	    int getObjectPointer(LogicalID* lid, AccessMode mode, ObjectPointer* &p);
+      private:
+	    void setSM(StoreManager*);
+	    void setID(int);
+}; 
 
-	/**
-	 *	TransactionManager - fabryka obiektow Transaction, kazdy z Executor'ow prosi go o nowa transakcje
-	 *      Sigleton Design Pattern
-	 */
-	class TransactionManager
-	{
-	   StoreManager* storeMgr;
-	   static TransactionManager *tranMgr;   
-	   public:
-	      TransactionManager();
-	      static int createTransaction(Transaction* &tr);
-	      int init(StoreManager *strMgr);
-	};
+/**
+ *	TransactionManager - fabryka obiektow Transaction, kazdy z Executor'ow prosi go o nowa transakcje
+ *      Sigleton Design Pattern
+ */
+class TransactionManager
+{
+   StoreManager* storeMgr;
+   static TransactionManager *tranMgr;   
+   public:
+      TransactionManager();
+      static int createTransaction(Transaction* &tr);
+      int init(StoreManager *strMgr);
+};
 
-	/**
-	 *	LockManager - implementuje tablice zamkow, wywolywany przez dana Transakcje 
-	 */
-	class LockManager
-	{ 
-	      public:      
-	      static int lock(LogicalID* lid, TransactionID* tid, AccessMode mode);
-	};
+/**
+ *	LockManager - implementuje tablice zamkow, wywolywany przez dana Transakcje 
+ */
+class LockManager
+{ 
+      public:      
+      static int lock(LogicalID* lid, TransactionID* tid, AccessMode mode);
+};
 
 };
 
