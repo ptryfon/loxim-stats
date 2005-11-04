983aa6ec440f9127c7d167d793602a75597f2c17
jk209476 <jk209476@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-04 10:54:09 +0000

    Kosmetyczne zmiany interfejsow - w porozumieniu z innymi modulami.

--- TransactionManager/Transaction.cpp
+++ TransactionManager/Transaction.cpp
@@ -1,80 +1,80 @@
 #include<stdio.h>
 #include "Transaction.h"
+#include "../Lock/Lock.h"
+
+using namespace LockMgr;
 
 namespace TManager
 {
-/**
- *	TransactionID - obiekt jednoznacznie identyfikujacy transakcje
- */
-TransactionID::TransactionID(int id)
-{
-    printf("TransactionID");
-};
 
-/**
- *   Transaction - obiekt w ramach ktorego dziala Executor
- */
-Transaction::Transaction() {};
+	/**
+	 *	TransactionID - obiekt jednoznacznie identyfikujacy transakcje
+	 */
+	TransactionID::TransactionID(int id)
+	{
+	    printf("TransactionID");
+	};
 
-Transaction::Transaction(TransactionID* tId)
-{
-    printf("Transaction");
-    tid = tId;
-};
+	/**
+	 *   Transaction - obiekt w ramach ktorego dziala Executor
+	 */
+	Transaction::Transaction() {};
 
-void Transaction::setSM(StoreManager* stmg)
-{
-    sm = stmg;
-}
+	Transaction::Transaction(TransactionID* tId)
+	{
+	    printf("Transaction");
+	    tid = tId;
+	};
 
-		
-int Transaction::getObjectPointer(LogicalID*  lid, AccessMode mode, ObjectPointer* &p)
-{
-    ObjectPointer *tmp;
+	void Transaction::setSM(StoreManager* stmg)
+	{
+	    sm = stmg;
+	}
 
-    printf("Transaction: getProxy\n");fflush(stdout);
-    LockManager::lock(lid, tid, mode);
-    //wywolanie Store'a
-    tmp = sm->getObject( tid, lid, mode );
-    return 0; 
-};
+			
+	int Transaction::getObjectPointer(LogicalID*  lid, AccessMode mode, ObjectPointer* &p)
+	{
+	    ObjectPointer *tmp;
+
+	    printf("Transaction: getProxy\n");fflush(stdout);
+	    LockManager::getHandle()->lock(lid, tid, mode);
+	    //wywolanie Store'a
+	    tmp = sm->getObject( tid, lid, mode );
+	    return 0; 
+	};
+
+	int Transaction::commit()
+	{
+	    printf("Transaction commited\n"); fflush(stdout);
+	    return 0;
+	}
+	    
+	/**
+	 *	TransactionManager - fabryka obiektow Transaction, kazdy z Executor'ow prosi go o nowa transakcje
+	 */
+
+	TransactionManager::TransactionManager() {}
+        
+	TransactionManager* TransactionManager::tranMgr = new TransactionManager();
+	
+	TransactionManager* TransactionManager::getHandle() { return tranMgr; };
+	
+	int TransactionManager::createTransaction(Transaction* &tr)
+	{
+	    printf("TransactionManager::createTransaction 0\n");fflush(stdout);
+	    tr = new Transaction( new TransactionID(1));
+	    tr->setSM(tranMgr->storeMgr);
+	    return 0;           
+	}              
+
+	int TransactionManager::init(StoreManager *strMgr)
+	{
+	    printf("TransactionManager - stworzony");
+	    storeMgr = strMgr;
+	    return 0;
+	}
 
 
-/**
- *	TransactionManager - fabryka obiektow Transaction, kazdy z Executor'ow prosi go o nowa transakcje
- */
 
-TransactionManager::TransactionManager() {
-  if (tranMgr != NULL) {
-    fprintf(stderr, "Only one instance of TransactionManager allowed");
-    exit(1);
-  }
-}
 
-int TransactionManager::createTransaction(Transaction* &tr)
-{
-    printf("TransactionManager::createTransaction 0\n");fflush(stdout);
-    tr = new Transaction( new TransactionID(1));
-    tr->setSM(tranMgr->storeMgr);
-    return 0;           
-}              
-
-int TransactionManager::init(StoreManager *strMgr)
-{
-    printf("TransactionManager - stworzony");
-    storeMgr = strMgr;
-    tranMgr = this;
-    return 0;
-}
-    
-TransactionManager* TransactionManager::tranMgr = NULL;
-
-/**
- *	LockManager - implementuje tablice zamkow, wywolywany przez dana Transakcje 
- */
-int LockManager::lock(LogicalID* lid, TransactionID* tid, AccessMode mode)
-{
-    printf("LockManager\n");fflush(stdout);
-    return 0;
-};
 };
