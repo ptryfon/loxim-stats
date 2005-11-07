07d1193e56c9eddd837c398bb0d221a168d0d549
ct201224 <ct201224@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-07 17:14:13 +0000

    SBQLCli.cpp - dostosowanie do Driver.a; Poprawiony Makefile w SBQLCli i w Server

--- SBQLCli/SBQLCli.cpp
+++ SBQLCli/SBQLCli.cpp
@@ -10,26 +10,23 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	if (argc != 3) {
-		cout << "SBQLCli a b" << endl;
+		cout << "SBQLCli host port" << endl;
 		return 0;
 	}
 	
 	char* host = argv[1];
 	int   port = atoi(argv[2]);
 	Connection* con;
-	try {
-		con = DriverManager::getConnection(host, port);
-	} catch (exception e) {
-		cout << "Can't connect" << endl;
-		cout << e.what();
-	}
+	con = DriverManager::getConnection(host, port);
+
 	
 	char input[50];
 	cout << "SBQLCli ver 0.0.0.0.0.1 \n";
 	cout << " > ";
 	while (cin.getline(input,50)) {
 		ResultSet* result = con->execute(input);
-		cout << result->toString();
+		//cout << result->toString();
+		cout << "Dostalem wynik \n";
 		cout << endl << " > ";
 		//cout << input << "\n";
 	};
