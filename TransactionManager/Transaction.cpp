16b9bee59eb7e74bf1697addb1d13a1d63244286
jk209476 <jk209476@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-12 20:19:53 +0000

    Dodanie kolku nowych metod

--- TransactionManager/Transaction.cpp
+++ TransactionManager/Transaction.cpp
@@ -1,5 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <vector>
+
 #include "Transaction.h"
+#include "Semlib.h"
 #include "../Lock/Lock.h"
 
 using namespace LockMgr;
@@ -7,40 +13,35 @@ using namespace LockMgr;
 namespace TManager
 {
 
-	/**
-	 *	TransactionID - obiekt jednoznacznie identyfikujacy transakcje
-	 */
+/*______TransactionID________________________________________*/
 	TransactionID::TransactionID(int id)
 	{
 	    printf("TransactionID");
+	    this->id = id;
 	};
 
-
-
-	/**
-	 *   Transaction - obiekt w ramach ktorego dziala Executor
-	 */
+/*______Transaction_________________________________________*/
 	Transaction::Transaction() {};
 
-	Transaction::Transaction(TransactionID* tId)
+	Transaction::Transaction(TransactionID* tid)
 	{
 	    printf("Transaction");
-	    tid = tId;
+	    this->tid = tid;
+	    tm = TransactionManager::getHandle();
+	    lm = LockManager::getHandle();
 	};
 
 	void Transaction::setSM(StoreManager* stmg)
 	{
 	    sm = stmg;
-	}
-
+	};
 			
-	int Transaction::getObjectPointer(LogicalID*  lid, AccessMode mode, ObjectPointer* &p)
+	int Transaction::getObjectPointer(LogicalID* lid, AccessMode mode, ObjectPointer* &p)
 	{
 	    ObjectPointer *tmp;
 
 	    printf("Transaction: getProxy\n"); fflush(stdout);
-	    LockManager::getHandle()->lock(lid, tid, mode);
-	    //wywolanie Store'a
+	    lm->lock(lid, tid, mode);
 	    tmp = sm->getObject( tid, lid, mode );
 	    //p = tmp;
 	    return 0; 
@@ -52,42 +53,40 @@ namespace TManager
 
 	    printf("Transaction: create\n"); fflush(stdout);
 	    tmp = sm->createObject( tid, name, value );
-	    // rozszerzenie zamkow
-	    //LockManager::getHandle()->createLock(tmp->getLogicalID());
+	    // locking object
 	    return 0; 
 	}
 
 	int Transaction::deleteObject(ObjectPointer* object)
 	{
 	    printf("Transaction: delete\n"); fflush(stdout);
-	    // zablokowanie obiektu na wylacznosc
-	    LockManager::getHandle()->lock(object->getLogicalID(), tid, Write);
+	    /* exclusive lock for this object */
+	    lm->lock(object->getLogicalID(), tid, Write);
 	    sm->deleteObject(tid, object);
-	    // skasowanie zamka
-	    //LockManager::getHandle()->deleteLock(tmp->getLogicalID());
+	    
 	    return 0; 
 	}
 
 	int Transaction::commit()
 	{
 	    printf("Transaction commited\n"); fflush(stdout);
-	    return 0;
+	    return tm->commit(this);
 	}
 
 	int Transaction::abort()
 	{
 	    printf("Transaction aborted\n"); fflush(stdout);
-	    return 0;
+	    return tm->abort(this);
 	}
 
-
-
 	    
-	/**
-	 *	TransactionManager - fabryka obiektow Transaction, kazdy z Executor'ow prosi go o nowa transakcje
-	 */
+/*______TransactionManager______________________________________ */
 
-	TransactionManager::TransactionManager() {}
+	TransactionManager::TransactionManager() 
+	{
+	    if ((mutex = semget(SEMKEY1, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0)	    
+		 printf("Error in semget\n"); //exit(1); }	     
+	};
         
 	TransactionManager* TransactionManager::tranMgr = new TransactionManager();
 	
@@ -96,7 +95,15 @@ namespace TManager
 	int TransactionManager::createTransaction(Transaction* &tr)
 	{
 	    printf("TransactionManager::createTransaction 0\n");fflush(stdout);
-	    tr = new Transaction( new TransactionID(1));
+	    
+	    P(mutex);
+		int currentId = transactionId;
+		transactionId++;
+	    V(mutex);
+	    
+	    TransactionID* tid = new TransactionID(currentId);
+	    addActiveTransaction(tid);
+	    tr = new Transaction(tid);
 	    tr->setSM(tranMgr->storeMgr);
 	    return 0;           
 	}              
@@ -108,7 +115,30 @@ namespace TManager
 	    return 0;
 	}
 
-
-
-
-};
+	void TransactionManager::addActiveTransaction(TransactionID* tid)
+	{
+	    // adding new Transaction to internal data structure - hashmap 
+	}
+	
+	vector<int>* TransactionManager::getActiveTransactions()
+	{
+	    vector<int>* v = new vector<int>(0);
+	    /* block creating new transactions */
+	    P(mutex);
+		// get active transactions from internal data structure
+	    V(mutex);
+	    return v;  
+	}
+	
+	int TransactionManager::commit(Transaction* tr)
+	{
+	    delete tr;
+	    return 0;
+	}
+	
+	int TransactionManager::abort(Transaction* tr)
+	{
+	    delete tr;
+	    return 0;
+	}
+}
