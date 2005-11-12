0b857919dc8dd92f74e99b7d49e99552c9018bb8
am209482 <am209482@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-12 11:21:33 +0000

    *** empty log message ***

--- QueryExecutor/QueryResult.h
+++ QueryExecutor/QueryResult.h
@@ -1,13 +1,23 @@
 #ifndef _QUERYRESULT_H_
 #define _QUERYRESULT_H_
 
+#include <list>
+
+#include "Store/Store.h"
+
+using std::list;
+
 namespace QExecutor {
 
 	class QueryResult
 	{
+		private:
+			list <LogicalID *> res;
+			int addNext(LogicalID *lid); //adds next LogicalID* to result
 		public:    
-			QueryResult() {};  
-			~QueryResult();   
+			QueryResult();
+			~QueryResult();
+			int getNext(LogicalID *lid); //returns next result (LogicalID*)
 	};
 
 }
