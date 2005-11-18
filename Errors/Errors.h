500b4e2a3223aaa1bb0dc90d1ba28c37c1ba44ce
baggins <baggins@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-18 09:52:35 +0000

    - troche definicji

--- Errors/Errors.h
+++ Errors/Errors.h
@@ -9,7 +9,31 @@ namespace Errors {
 	// Nazwy bledow:
 	// <Namespace z ktorego blad pochodzi><Krotka nazwa>
 	// np
-	// StoreNoSpace		-ENOSPC
+	// StoreNoSpace		ENOSPC
+#define ErrorClassBackup		 0x1000
+
+#define	ErrorClassConfig		 0x2000
+
+#define	ErrorClassDriver		 0x4000
+
+#define	ErrorClassErrors		 0x8000
+#define ErrorsNoFile			1 | ErrorClassErrors
+
+#define	ErrorClassLock			0x10000
+
+#define	ErrorClassLog			0x12000
+
+#define	ErrorClassQueryExecutor		0x14000
+
+#define	ErrorClassQueryParser		0x18000
+
+#define	ErrorClassSBQLCli		0x20000
+
+#define	ErrorClassServer		0x22000
+
+#define	ErrorClassStore			0x24000
+
+#define	ErrorClassTransactionManager	0x28000
 }
 
 #endif
