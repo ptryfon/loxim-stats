bf4e700df5d9dd88eae36126fbfcae7019ef2ff2
baggins <baggins@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-18 09:57:29 +0000

    - kosmetyka

--- Errors/Errors.h
+++ Errors/Errors.h
@@ -11,29 +11,19 @@ namespace Errors {
 	// np
 	// StoreNoSpace		ENOSPC
 #define ErrorClassBackup		 0x1000
-
 #define	ErrorClassConfig		 0x2000
-
 #define	ErrorClassDriver		 0x4000
-
 #define	ErrorClassErrors		 0x8000
-#define ErrorsNoFile			1 | ErrorClassErrors
-
 #define	ErrorClassLock			0x10000
-
 #define	ErrorClassLog			0x12000
-
 #define	ErrorClassQueryExecutor		0x14000
-
 #define	ErrorClassQueryParser		0x18000
-
 #define	ErrorClassSBQLCli		0x20000
-
 #define	ErrorClassServer		0x22000
-
 #define	ErrorClassStore			0x24000
-
 #define	ErrorClassTransactionManager	0x28000
+
+#define ErrorsNoFile			1 | ErrorClassErrors
 }
 
 #endif
