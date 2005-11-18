076bdc768005f5fd1ac0b7b43b86e67b2518da44
am209482 <am209482@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-18 09:35:27 +0000

    *** empty log message ***

--- QueryExecutor/QueryExecutor.cpp
+++ QueryExecutor/QueryExecutor.cpp
@@ -39,18 +39,14 @@ int QueryExecutor::queryResult(TreeNode *tree, QueryResult *result) {
 	if (TransactionManager::getHandle()->createTransaction(tr) != 0) {
 		fprintf(stderr, "Error in createTransaction\n");
 	}
-	//fprintf(stderr, "QueryExecutor asking Store for proxy object to calculate query\n");
-	//UWAGA! PONIZSZE INSTRUKCJE SA TU TYLKO TYMCZASOWO - TRZEBA JE WKLEIC NA SWITCHA SKLADNIOWEGO...
-	//---- tego jeszcze nie jestem pewien - nie do konca zrozumialem TMa
 	
 	treeNodeType = tree->type();
 	if (treeNodeType == "name") {
 		name = tree->getName();
 		if (tr->getRoots(name, vec) != 0) {
-		    fprintf(stderr, "Error in getRoots\n");
-		    return -1;
+			fprintf(stderr, "Error in getRoots\n");
+			return -1;
 		}
-//		result = new QueryResult;
 		vecSize = vec->size();
 		for (int i = 0; i < vecSize; i++ ) {
    			optr = vec->at(i);
@@ -61,16 +57,15 @@ int QueryExecutor::queryResult(TreeNode *tree, QueryResult *result) {
 	}
 	else if (treeNodeType == "create") {
 			name = tree->getName();
-			value = ((tree->getArg())->getValue());
+			value = (DataValue *) ((tree->getArg())->getValue());
 			if (tr->createObject(name, value, optr) != 0) {
-			    fprintf(stderr, "Error in createObject\n");
-			    return -1;
+				fprintf(stderr, "Error in createObject\n");
+				return -1;
 			}
 			if (tr->addRoot(optr) != 0) {
-			    fprintf(stderr, "Error in addRoot\n");
-			    return -1;
+				fprintf(stderr, "Error in addRoot\n");
+				return -1;
 			}
-//			result = new QueryResult;
 			result->addNext(optr->getLogicalID());
 			return 0;
 		}
@@ -98,18 +93,6 @@ int QueryExecutor::queryResult(TreeNode *tree, QueryResult *result) {
 	    default: { break;}
 	    }
 	*/
-	
-	
-	//---- odpowiednik wywolania CREATE name(value);
-	//---- optr jest *wynikiem* tej metody!
-
-	
-	//---- odpowiednik wywolania CREATE name; (z pusta wartoscia)
-	//---- optr jest *wynikiem* tej metody!
-	//KONIEC
-	// !!!!! optr - tu jest wynik naszego zapytania (musimy zwrocic jego LogicalID* - funkcja optr.getLogicalID() )
-	
-	//tutaj ladnie ukladamy QueryResult
 }
 
 QueryExecutor::~QueryExecutor() {}
