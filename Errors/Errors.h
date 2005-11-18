0a3fc453fe2f6963dca2c2e626b8bb89a374bd6b
baggins <baggins@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-18 10:08:22 +0000

    - wersja sensowniejsza

--- Errors/Errors.h
+++ Errors/Errors.h
@@ -3,27 +3,26 @@
 
 #include <errno.h>
 
-//using namespace std;
+// Sposob uzycia:
+// return ENoFile | ErrLogs
+// jesli istnieje kod bledu w errno.h nalezy go uzywac:
+// return ENOSPC | ErrLogs
 
 namespace Errors {
-	// Nazwy bledow:
-	// <Namespace z ktorego blad pochodzi><Krotka nazwa>
-	// np
-	// StoreNoSpace		ENOSPC
-#define ErrorClassBackup		 0x1000
-#define	ErrorClassConfig		 0x2000
-#define	ErrorClassDriver		 0x4000
-#define	ErrorClassErrors		 0x8000
-#define	ErrorClassLock			0x10000
-#define	ErrorClassLog			0x12000
-#define	ErrorClassQueryExecutor		0x14000
-#define	ErrorClassQueryParser		0x18000
-#define	ErrorClassSBQLCli		0x20000
-#define	ErrorClassServer		0x22000
-#define	ErrorClassStore			0x24000
-#define	ErrorClassTransactionManager	0x28000
+#define ErrBackup		 0x1000
+#define	ErrConfig		 0x2000
+#define	ErrDriver		 0x4000
+#define	ErrErrors		 0x8000
+#define	ErrLockMgr		0x10000
+#define	ErrLogs			0x12000
+#define	ErrQExecutor		0x14000
+#define	ErrQParser		0x18000
+#define	ErrSBQLCli		0x20000
+#define	ErrServer		0x22000
+#define	ErrStore		0x24000
+#define	ErrTManager		0x28000
 
-#define ErrorsNoFile			1 | ErrorClassErrors
+#define ENoFile			1
 }
 
 #endif
