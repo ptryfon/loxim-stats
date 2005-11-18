3a617b2de6ee98a3cd8fe0a41fa827ea42e33c3e
ct201224 <ct201224@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-18 09:37:48 +0000

    *** empty log message ***

---
+++ Driver/Result.cpp
@@ -0,0 +1,46 @@
+
+#include <sstream>
+#include "Result.h"
+
+/** 
+ * Result
+ */
+void Result::toStream(ostream& os) const
+{
+	os << "Result";
+}
+
+ostream& operator<<(ostream& os, Result& result) {
+	//result.toStream(os);
+	os << "operator";
+	return os;
+}
+
+
+/**
+ * ResultBag
+ */
+ResultBag::ResultBag(unsigned long size)
+{
+}
+
+ResultBag::~ResultBag()
+{
+}
+
+
+/**
+ * ResultReference
+ */
+ResultReference::ResultReference(int val)
+{
+	value = val;
+}
+
+string ResultReference::toString()
+{
+	ostringstream stream;
+	stream << value;
+	return stream.str();
+}
+
