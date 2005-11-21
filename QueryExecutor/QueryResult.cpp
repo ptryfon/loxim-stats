d2a0ee86e5ea82d7a178ff12297f2522ee25aa69
dg203087 <dg203087@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-21 01:25:11 +0000

    *** empty log message ***

--- QueryExecutor/QueryResult.cpp
+++ QueryExecutor/QueryResult.cpp
@@ -1,34 +1,141 @@
-/* Query Executor
-   10.2005 - 01.2006
-
-   Authors:
-   Beata Golichowska
-   Dariusz Gryglas
-   Adam Michalik
-   Maja Perycz */
-   
-#include <stdio.h>
 #include <vector>
-
 #include "QueryResult.h"
+
+#include <string>
+#include <vector>
 #include "Store/Store.h"
+
+using namespace std;
 
 namespace QExecutor {
 
-	QueryResult::QueryResult() {};
-	QueryResult::~QueryResult() {};
+//constructors
+QuerySequenceResult::QuerySequenceResult() 			{}
+QuerySequenceResult::QuerySequenceResult(unsigned int size)	{}
+QuerySequenceResult::QuerySequenceResult(vector<QueryResult*> s){ seq = s; }
+QueryBagResult::QueryBagResult() 				{}
+QueryBagResult::QueryBagResult(unsigned int size)		{}
+QueryBagResult::QueryBagResult(vector<QueryResult*> b)		{ bag = b; }
+QueryStructResult::QueryStructResult() 				{}
+QueryStructResult::QueryStructResult(unsigned int size)		{}
+QueryStructResult::QueryStructResult(vector<QueryResult*> s)	{ str = s; }
+QueryBinderResult::QueryBinderResult(string n, QueryResult* r) 	{ name=n; item=r; }
+QueryStringResult::QueryStringResult(string v) 			{ value=v; }
+QueryIntResult::QueryIntResult(int v) 				{ value=v; }
+QueryDoubleResult::QueryDoubleResult(double v) 			{ value=v; }
+QueryBoolResult::QueryBoolResult(bool v) 			{ value=v; }
+QueryReferenceResult::QueryReferenceResult(LogicalID* v) 	{ value=v; }
+QueryNothingResult::QueryNothingResult() 			{}
 
-	int QueryResult::addNext(LogicalID *lid) {
-		res.push_front(lid);
-		return 0;
-	}
+//function clone()
+QueryResult* QuerySequenceResult::clone()	{ return new QuerySequenceResult(seq); }
+QueryResult* QueryBagResult::clone()		{ return new QueryBagResult(bag); }
+QueryResult* QueryStructResult::clone()		{ return new QueryStructResult(str); }
+QueryResult* QueryBinderResult::clone()		{ return new QueryBinderResult(name, (QueryResult*)item->clone()); }
+QueryResult* QueryStringResult::clone()		{ return new QueryStringResult(value); }
+QueryResult* QueryIntResult::clone()		{ return new QueryIntResult(value); }
+QueryResult* QueryDoubleResult::clone()		{ return new QueryDoubleResult(value); }
+QueryResult* QueryBoolResult::clone()		{ return new QueryBoolResult(value); }
+QueryResult* QueryReferenceResult::clone()	{ return new QueryReferenceResult(value); }
+QueryResult* QueryNothingResult::clone()	{ return new QueryNothingResult(); }
 
-	int QueryResult::getNext(LogicalID *lid) {
-		if (res.size() != 0) {
-			lid = res.front();
-			res.pop_front();
-			return 0;
-		}
-		return -1;
-	};
+//function type() - returns type of a result
+int QueryResult::type()			{ return QueryResult::QRESULT; }
+int QuerySequenceResult::type()		{ return QueryResult::QSEQUENCE; }
+int QueryBagResult::type()		{ return QueryResult::QBAG; }
+int QueryStructResult::type()		{ return QueryResult::QSTRUCT; }
+int QueryBinderResult::type()		{ return QueryResult::QBINDER; }
+int QueryStringResult::type()		{ return QueryResult::QSTRING; }
+int QueryIntResult::type()		{ return QueryResult::QINT; }
+int QueryDoubleResult::type()		{ return QueryResult::QDOUBLE; }
+int QueryBoolResult::type()		{ return QueryResult::QBOOL; }
+int QueryReferenceResult::type()	{ return QueryResult::QREFERENCE; }
+int QueryNothingResult::type()		{ return QueryResult::QNOTHING; }
+
+//function collection() - returns true if result is a collection, false if not
+bool QuerySequenceResult::collection()	{ return true; }
+bool QueryBagResult::collection()	{ return true; }
+bool QueryStructResult::collection()	{ return true; }
+bool QueryBinderResult::collection()	{ return false; }
+bool QueryStringResult::collection()	{ return false; }
+bool QueryIntResult::collection()	{ return false; }
+bool QueryDoubleResult::collection()	{ return false; }
+bool QueryBoolResult::collection()	{ return false; }
+bool QueryReferenceResult::collection()	{ return false; }
+bool QueryNothingResult::collection()	{ return false; }
+
+//function isEmpty() - returns false if a result contains some other results, true if does not
+bool QuerySequenceResult::isEmpty()	{ return seq.empty(); }
+bool QueryBagResult::isEmpty()		{ return bag.empty(); }
+bool QueryStructResult::isEmpty()	{ return str.empty(); }
+bool QueryBinderResult::isEmpty()	{ if (item != NULL) {return false;} else {return true;}; }
+bool QueryStringResult::isEmpty()	{ return true; }
+bool QueryIntResult::isEmpty()		{ return true; }
+bool QueryDoubleResult::isEmpty()	{ return true; }
+bool QueryBoolResult::isEmpty()		{ return true; }
+bool QueryReferenceResult::isEmpty()	{ return true; }
+bool QueryNothingResult::isEmpty()	{ return true; }
+
+//function size() - returns how many other results does a result contain
+int QuerySequenceResult::size()		{ return seq.size(); }
+int QueryBagResult::size()		{ return bag.size(); }
+int QueryStructResult::size()		{ return str.size(); }
+int QueryBinderResult::size()		{ if (item != NULL) {return 1;} else {return 0;}; }
+int QueryStringResult::size()		{ return 0; }
+int QueryIntResult::size()		{ return 0; }
+int QueryDoubleResult::size()		{ return 0; }
+int QueryBoolResult::size()		{ return 0; }
+int QueryReferenceResult::size()	{ return 0; }
+int QueryNothingResult::size()		{ return 0; }
+
+//accesors for collection result classes - sequence, bag, sruct
+void QuerySequenceResult::addResult(QueryResult *r)	{ seq.push_back(r); }
+int QuerySequenceResult::getResult(QueryResult *r)	{ if (seq.empty()) { return -1; } 
+							else { r=(seq.front()); it=(seq.begin()); seq.erase(it);}; return 0; }
+
+void QueryBagResult::addResult(QueryResult *r)		{ bag.push_back(r); }
+int QueryBagResult::getResult(QueryResult *r)		{ if (bag.empty()) { return -1; } 
+							else { r=(bag.front()); it=(bag.begin()); bag.erase(it);}; return 0; }
+
+void QueryStructResult::addResult(QueryResult *r)	{ str.push_back(r); }
+int QueryStructResult::getResult(QueryResult *r)	{ if (str.empty()) { return -1; } 
+							else { r=(str.front()); it=(str.begin()); str.erase(it);}; return 0; }
+
+//accesors for single value type result classes - binder, string, int, double, bool, reference
+QueryResult* QueryBinderResult::getItem()		{ return item; }
+void QueryBinderResult::setItem(QueryResult *r)		{ item = r; }
+string QueryBinderResult::getName()			{ return name; }
+void QueryBinderResult::setName(string n)		{ name = n; }
+
+string QueryStringResult::getValue()			{ return value; }
+void QueryStringResult::setValue(string v)		{ value = v; }
+
+int QueryIntResult::getValue()				{ return value; }
+void QueryIntResult::setValue(int v)			{ value = v; }
+
+double QueryDoubleResult::getValue()			{ return value; }
+void QueryDoubleResult::setValue(double v)		{ value = v; }
+
+bool QueryBoolResult::getValue()			{ return value;}
+void QueryBoolResult::setValue(bool v)			{ value = v; }
+
+LogicalID* QueryReferenceResult::getValue()		{ return value; }
+void QueryReferenceResult::setValue(LogicalID* v)	{ value = v; }
+
 }
+
+/* TODO 
+- zglosic blad w funkcji getResult pojawiajacy sie gdy ktos sprubuje pobrac cos z pustej kolekcji
+- po przydzieleniu errno poprawic return -1 na return errno w tej funkcji
+
+- zastanowic sie jak naprawde powinny wygladac i zachowywac sie bag, sequence i struct
+- zmienic ich implementacje
+
+- opracowac operatory ==, >, < etc. +, -, etc...
+- byc moze dodac operator at() do kolekcji
+- moze takze dodac replace(i), remove(i)
+
+- do rozwazenia, czy zrobimy osobna klase na stos czy wykorzystamy ktoryz z typow result,
+- do rozwazenie czy operacje typu suma zbiorów, przeciecie, i inne podobne jakie bedziemy wykonywac na wynikach maja byc zdefiniowane
+  w QueryExecutorze czy tez moga to byc wbudowane metody poszczególnych resultów */
+   
--- QueryExecutor/QueryResult.h
+++ QueryExecutor/QueryResult.h
@@ -2,128 +2,240 @@
 #define _QUERYRESULT_H_
 
 #include <string>
+#include <vector>
 #include "Store/Store.h"
 
-//using namespace std;
+using namespace std;
 
 namespace QExecutor {
 
-class QueryResult {
- public:
-  enum QueryResultType { QRESULT, QSEQUENCE, QBAG, 
-                         QSTRUCT, QBINDER,  QSTRING, 
-                         QINT, QDOUBLE, QBOOL, 
-                         QREFERENCE, QNOTHING };
-  virtual bool operator==(QueryResult& r)=0;
-  virtual int type()=0;
-  virtual QueryResult getObject()=0;
-  virtual QueryResult addObject()=0;
-  virtual QueryResult* clone()=0;
-  virtual ~QueryResult() {}
+class QueryResult
+{
+public:
+	enum QueryResultType {
+		QRESULT,
+		QSEQUENCE,
+		QBAG,
+		QSTRUCT,
+		QBINDER,
+		QSTRING,
+		QINT,
+		QDOUBLE,
+		QBOOL,
+		QREFERENCE,
+		QNOTHING
+		};
+		
+	//virtual bool operator==(QueryResult& r)=0;
+	virtual QueryResult* clone()=0;
+	
+	int type();
+	virtual bool collection()=0;
+	virtual bool isEmpty()=0;
+	virtual int size()=0;
 };
 
-class QuerySequenceResult : public QueryResult {
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  virtual void operator+=(QueryResult& r){}
-  virtual QueryResult& operator[](int p){ return *(this->clone());}
-  virtual int type() { return QueryResult::QSEQUENCE; }
+
+class QuerySequenceResult : public QueryResult
+{
+protected:
+	vector<QueryResult*> seq;
+	vector<QueryResult*>::iterator it;
+public:
+	QuerySequenceResult ();
+	QuerySequenceResult (unsigned int size);
+	QuerySequenceResult (vector<QueryResult*> s);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	//virtual void operator+=(QueryResult& r){};
+	//virtual QueryResult& operator[](int p){ return *(this->clone());};
+	QueryResult* clone();
+	virtual ~QuerySequenceResult() {
+		if ((seq.size()) > 0) {
+			for (unsigned int i = 0; i < (seq.size()); i++ ) { delete (seq.at(i)); };
+		}; 
+	};
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
+	void addResult(QueryResult *r);
+	int getResult(QueryResult *r);
 };
 
-class QueryBagResult : public QueryResult {
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  virtual void operator+=(QueryResult& r){}
-  virtual int type() { return QueryResult::QBAG; }      
+class QueryBagResult : public QueryResult
+{
+protected:
+	vector<QueryResult*> bag;
+	vector<QueryResult*>::iterator it;
+public:
+	QueryBagResult ();
+	QueryBagResult (unsigned int size);
+	QueryBagResult (vector<QueryResult*> b);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	//virtual void operator+=(QueryResult& r){};
+	QueryResult* clone();
+	virtual ~QueryBagResult() {
+		if ((bag.size()) > 0) {
+			for (unsigned int i = 0; i < (bag.size()); i++ ) { delete (bag.at(i)); };
+		}; 
+	};
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
+	void addResult(QueryResult *r);
+	int getResult(QueryResult *r);
 };
 
-class QueryStructResult : public QueryResult {
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  virtual void operator+=(QueryResult& r){ }
-  virtual int type() { return QueryResult::QSTRUCT; }     
+
+class QueryStructResult : public QueryResult
+{
+protected:
+	vector<QueryResult*> str;
+	vector<QueryResult*>::iterator it;
+public:
+	QueryStructResult ();
+	QueryStructResult (unsigned int size);
+	QueryStructResult (vector<QueryResult*> s);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	//virtual void operator+=(QueryResult& r){};
+	QueryResult* clone();
+	virtual ~QueryStructResult() {
+		if ((str.size()) > 0) {
+			for (unsigned int i = 0; i < (str.size()); i++ ) { delete (str.at(i)); };
+		}; 
+	};
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
+	void addResult(QueryResult *r);
+	int getResult(QueryResult *r);
 };
 
-class QueryBinderResult : public QueryResult {
- protected:
-  string name;
-  QueryResult* item;
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL;}
-  QueryBinderResult(string _name, QueryResult* _r) : name(_name), item(_r) {}
-  string getName() { return name; }
-  QueryResult* getItem() { return item; }
-  virtual int type() { return QueryResult::QBINDER; }      
-  virtual ~QueryBinderResult() { if (item != NULL) delete item; }  
+
+class QueryBinderResult : public QueryResult
+{
+protected:
+	string name;
+	QueryResult* item;
+public:
+	QueryBinderResult(string n, QueryResult* r);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	QueryResult* clone();
+	virtual ~QueryBinderResult() { if (item != NULL) delete item; };
+	string getName();
+	void setName(string n);
+	QueryResult* getItem();
+	void setItem(QueryResult* r);
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
-class QueryStringResult : public QueryResult {
- protected:
-  string value;
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  QueryStringResult(string  _value) : value(_value) {}
-  string getValue() { return value; }
-  virtual int type() { return QueryResult::QSTRING; }
+
+class QueryStringResult : public QueryResult
+{
+protected:
+	string value;
+public:
+	QueryStringResult(string  v);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	QueryResult* clone();
+	virtual ~QueryStringResult() {};
+	string getValue();
+	void setValue(string v);
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
-class QueryIntResult : public QueryResult {
- protected:
-  int value;
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  QueryIntResult(int  _value) : value(_value) {}
-  int getValue() { return value; }
-  virtual int type() { return QueryResult::QINT; }
+
+class QueryIntResult : public QueryResult
+{
+protected:
+	int value;
+public:
+	QueryIntResult(int  v);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	QueryResult* clone();
+	virtual ~QueryIntResult() {};
+	int getValue();
+	void setValue(int v);
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
-class QueryDoubleResult : public QueryResult {
- protected:
-  double value;
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  QueryDoubleResult(double _value) : value(_value) {}
-  double getValue() { return value; }
-  virtual int type() { return QueryResult::QDOUBLE; }
+
+class QueryDoubleResult : public QueryResult
+{
+protected:
+	double value;
+public:
+	QueryDoubleResult(double v);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	QueryResult* clone();
+	virtual ~QueryDoubleResult() {};
+	double getValue();
+	void setValue(double v);
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
-class QueryBoolResult : public QueryResult {
- protected:
-  bool value;
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  QueryBoolResult(bool _value) : value(_value) {}
-  bool getValue() { return value; }
-  virtual int type() { return QueryResult::QBOOL; }
+class QueryBoolResult : public QueryResult
+{
+protected:
+	bool value;
+public:
+	QueryBoolResult(bool v);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	QueryResult* clone();
+	virtual ~QueryBoolResult() {};
+	bool getValue();
+	void setValue(bool v);
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
-class QueryReferenceResult : public QueryResult {
- protected:
-  LogicalID *value;
- public:
-  virtual bool operator==(QueryResult& r){ return false; }
-  virtual QueryResult* clone(){ return NULL; }
-  QueryReferenceResult(LogicalID* _value) : value(_value) {}
-  LogicalID* getValue() { return value; }
-  virtual int type() { return QueryResult::QREFERENCE; }     
-  virtual ~QueryReferenceResult() { if (value != NULL) delete value; }
+
+class QueryReferenceResult : public QueryResult
+{
+protected:
+	LogicalID *value;
+public:
+	QueryReferenceResult(LogicalID* v);
+	//virtual bool operator==(QueryResult& r){ return false; };
+	QueryResult* clone();
+	virtual ~QueryReferenceResult() { if (value != NULL) delete value; };
+	LogicalID* getValue();
+	void setValue(LogicalID* v);
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
-class QueryNothing {
- public:
-  virtual bool operator==(QueryResult& r) { return false; };
-  virtual int type() { return QueryResult::QNOTHING; };
-  virtual QueryResult* clone() {return NULL; };
-  virtual ~QueryNothing() {}
+/* You get this, as a result of non-select query like create, insert etc. */
+class QueryNothingResult : public QueryResult
+{
+public:
+	QueryNothingResult();
+	QueryResult* clone();
+	virtual ~QueryNothingResult() {};
+	int type();
+	bool collection();
+	bool isEmpty();
+	int size();
 };
 
 }
-#endif
+
+#endif  /* _QUERYRESULT_H_ */
