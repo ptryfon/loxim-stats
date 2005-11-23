7d5dfbcb4a91a07679187df9a8007e27c688754d
jk209476 <jk209476@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-23 19:55:01 +0000

    Poczatkowe metody do implementacji tablicy zamkow

--- Lock/Lock.cpp
+++ Lock/Lock.cpp
@@ -3,15 +3,77 @@
 
 namespace LockMgr{
 
+
+/*_____LockManager___________________________________________________*/
     LockManager* LockManager::lockMgr = new LockManager();
 
     int LockManager::lock(LogicalID* lid, TransactionID* tid, AccessMode mode)
-    {
-	printf("Object locked\n"); fflush(stdout);
-        return 0;
+    {	
+        
+	return 0;
     }
-
+    int LockManager::unlockAll(int transaction_id)
+    {
+	set<DBPhysicalID*>* locks = (*transaction_locks)[transaction_id];
+	for (set<DBPhysicalID*>::iterator iter = locks->begin();
+	     iter != locks->end(); iter++)
+	{
+	    unlock(*iter);
+	}
+	return 0;
+    }
+    int LockManager::unlock(DBPhysicalID* pid)
+    {
+	return 0;
+    }
     LockManager* LockManager::getHandle() { return lockMgr; }
     
-    LockManager::LockManager() {}
+    LockManager::LockManager() 
+    {
+	transaction_locks = new map<int , set<DBPhysicalID*>*>;
+	array_of_locks    = new map<DBPhysicalID*, SingleLock*>;
+    }
+
+
+/*_____SingleLock___________________________________________________*/    
+    SingleLock::SingleLock(int current, AccessMode mode)
+    {
+	this->current = current;
+	this->current_mode = mode;
+	wait_readers = new deque<int>(0);
+	wait_writers = new deque<int>(0);
+    }
+    void SingleLock::addReader(int reader_id)
+    {
+	wait_readers->push_back(reader_id);
+    }
+    void SingleLock::addWriter(int writer_id)
+    {
+	wait_writers->push_back(writer_id);
+    }
+    int SingleLock::getReader() 
+    { 
+	int reader = wait_readers->front();
+	wait_readers->pop_front();
+	return reader;
+    }
+    int SingleLock::getWriter()
+    {
+	int writer = wait_writers->front();
+	wait_writers->pop_front();
+	return writer;
+    }
+    int SingleLock::is_reader()
+    {
+	return !wait_readers->empty();
+    }
+    int SingleLock::is_writer()
+    {
+	return !wait_writers->empty();
+    }
+    void SingleLock::setCurrent(int current, AccessMode mode)
+    {
+	this->current = current;
+	this->current_mode = mode;
+    }
 }
--- Lock/Lock.h
+++ Lock/Lock.h
@@ -2,21 +2,59 @@
 #define _LOCK_
 
 namespace LockMgr { class LockManager; }
+
 #include "../Store/Store.h"
+#include "../Store/DBPhysicalID.h"
 #include "../TransactionManager/Transaction.h"
+
+#include <deque>
+#include <map>
+#include <set>
+#include <iterator>
+
 using namespace Store;
 using namespace TManager;
 
 namespace LockMgr {
 
-class LockManager{
-    private:
+    class SingleLock;
+    
+    class LockManager{
+	private:
 	    static LockManager* lockMgr;
     	    LockManager();
-    public: 
+	    /* from PhysicalId to structure of lock */
+	    map<Store::DBPhysicalID* , SingleLock* >* array_of_locks;
+	    /* from transactionId to list of PhysicalIds locked */
+	    map<int, set<DBPhysicalID*>* >* transaction_locks;
+	    int unlock(DBPhysicalID*);
+	public: 
 	    static LockManager* getHandle();
 	    int lock(LogicalID* lid, TransactionID* tid, AccessMode mode);
-};
+	    int unlockAll(int);
+    };
 
+    class SingleLock
+    {
+	private:
+	    /* current id transaction that locked an object */
+	    int current; 
+	    /* current lock mode: Read | Write */
+	    AccessMode current_mode;
+	    /* transaction ids that wait for reading an object */
+	    deque<int>* wait_readers;
+	    /* transaction ids that wait for writing an object */
+	    deque<int>* wait_writers;
+	    
+	public:
+	    SingleLock(int current, AccessMode mode);
+	    void addReader(int reader_id);
+	    void addWriter(int writer_id);
+	    int getReader();
+	    int getWriter();
+	    int is_reader();
+	    int is_writer();	    
+	    void setCurrent(int current, AccessMode mode);	    
+    };
 }
 #endif
