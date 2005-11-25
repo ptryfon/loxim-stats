a843dd44dae3621792815ca8fde6107abbd13973
ct201224 <ct201224@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-11-25 09:59:59 +0000

    Automatycznie wybierany jest localhost i port

--- SBQLCli/SBQLCli.cpp
+++ SBQLCli/SBQLCli.cpp
@@ -9,24 +9,37 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		cout << "SBQLCli host port" << endl;
-		return 0;
-	}
-	
-	char* host = argv[1];
-	int   port = atoi(argv[2]);
-	Connection* con;
-	con = DriverManager::getConnection(host, port);
+  
+  char* host = "localhost";
+  int   port = 6543;
 
-	
-	char input[50];
-	cout << "SBQLCli ver 0.0.0.0.1 \n";
-	cout << " > ";
-	while (cin.getline(input,50)) {
-		Result* result = con->execute(input);
-		cout << *result;
-		cout << endl << " > ";
-	};
-	return 0;		
+  /*if (argc != 3) {
+    cout << "SBQLCli host port" << endl;
+    return 0;
+    }*/
+
+  if (argc >= 2) {
+    host = argv[1];
+
+  }
+  if (argc == 3) {
+    port = atoi(argv[2]);
+  }
+
+
+  Connection* con;
+  con = DriverManager::getConnection(host, port);
+  
+  
+  char input[50];
+  cout << "SBQLCli ver 0.0.0.0.1 \n";
+  cout << " > ";
+  while (cin.getline(input,50)) {
+    Result* result = con->execute(input);
+    cout << *result;
+    cout << endl << " > ";
+  };
+
+  return 0;		
+
 }
