0c98455fa8ef0b5d1f89665ca8e15fa8ae6a88c9
js209298 <js209298@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-12-02 02:30:00 +0000

    proba dorobienia bledow

--- QueryParser/QueryParser.cpp
+++ QueryParser/QueryParser.cpp
@@ -4,32 +4,48 @@
 #include <FlexLexer.h>
 #include "TreeNode.h"
 #include "QueryParser.h"
+#include "../Errors/ErrorConsole.h"
+#include "../Errors/Errors.h"
+
 
 yyFlexLexer* lexer;
 extern QParser::TreeNode *d;
 int yyparse();
 
-using namespace std;
+// using namespace std;
+ using namespace Errors;	// FIXME 
 
 namespace QParser {
 
 	int QueryParser::parseIt(string query, TreeNode *&qTree)
 	{
+		ErrorConsole *ec = new ErrorConsole("Parser");
+		ec->init(1);
+		
 		stringstream ss (stringstream::in | stringstream::out);
 		ss << query;
 		lexer = new yyFlexLexer(&ss); 
 	        int res = yyparse();
-		if (res != 0)
-		    cout << "zapytanie nie sparsowane..." << endl;
-		else
-		    cout << "zapytanie sparsowane chyba ok" << endl;
+		if (res != 0){
+		    *ec << "zapytanie nie sparsowane robi return...\n";
+		    *ec << (ErrQParser | 5);	// zglosic jakis blad do Bagginsa ENotParsed ...    
+		    return (ErrQParser | 5);
+		} else
+		    *ec << "zapytanie sparsowane chyba ok\n";
 		delete lexer;
 		qTree = d;
-		printf( "po parsowaniu treeNode: %d. \n", qTree);
-		cout << "Odczyt z drzewka, ktore przekazuje:" << endl;
-		cout << "--------------------------------------" << endl;
-		qTree->putToString();
-		cout << "\n--------------------------------------" << endl;
+		
+		*ec << "po parsowaniu treeNode: ";
+		*ec << ((int) qTree);
+		*ec << "Odczyt z drzewka, ktore przekazuje: \n";
+		*ec << "----------------------------------------\n";
+		qTree->putToString();		// putToString uzywa coutow i dlatego inna kolejnosc wypisywania FIXME
+		*ec << "\n----------------------------------------\n";		
+		//printf( "po parsowaniu treeNode: %d. \n", qTree);
+		//cout << "88Odczyt z drzewka, ktore przekazuje:" << endl;
+		//cout << "88--------------------------------------" << endl;
+		//qTree->putToString();
+		//cout << "\n88--------------------------------------" << endl;
 		return 0;
 	}  
 }
