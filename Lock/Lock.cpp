ff5a1139b635699554729855b9ad2fd3676a9093
jk209476 <jk209476@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-12-04 18:14:53 +0000

    Struktura zamkow.

--- Lock/Lock.cpp
+++ Lock/Lock.cpp
@@ -1,42 +1,124 @@
 #include <stdio.h>
 #include "Lock.h"
+#include <iomanip>
+#include <algorithm>
 
-namespace LockMgr{
+using namespace Errors;
+namespace LockMgr
+{
+
+    /* returns true if db1 > db2  */
+   bool DBPhysicalIDCmp::operator() (const DBPhysicalID& db1, const DBPhysicalID& db2) const 
+   {
+	unsigned short f1, f2, p1, p2, o1, o2;
+	f1 = db1.getFile();
+	f2 = db2.getFile();
+	    
+	if (f1 != f2) return f1<f2;
+	    
+	p1 = db1.getPage();
+	p2 = db2.getPage();
+	    
+	if (p1 != p2) return p1<p2;
+	    
+	o1 = db1.getOffset();
+	o2 = db2.getOffset();
+	    
+	return o1<o2;	
+   }
+
+    /* returns true if tid1 > tid2 */
+   bool TransactionIDCmp::operator() (const TransactionID& tid1, const TransactionID& tid2) const 
+   {
+	return ( tid1.getId() > tid2.getId());
+   }
 
 
 /*_____LockManager___________________________________________________*/
+
+    /* the only instance of class */
     LockManager* LockManager::lockMgr = new LockManager();
 
+    LockManager* LockManager::getHandle() { return lockMgr; }
+
+    LockManager::LockManager() 
+    {
+	err = ErrorConsole();
+	sem_created = new set<int>;
+	transaction_locks = new TransactionIdMap;
+	map_of_locks      = new PhysicalIdMap; 
+	current_sem_key	  = 2000;
+	sem_created->insert(current_sem_key);
+	if ((mutex = create_sem(SEMKEY2)) < 0)
+	    err << "LockManager: Cannot create sem\n" ;
+	V(mutex);
+    }
+
+    LockManager::~LockManager()
+    {
+	for (set<int>::iterator iter = sem_created->begin();
+	     iter != sem_created->end(); iter++ )
+	
+	    release_sem(*iter);
+	
+    }    
     int LockManager::lock(LogicalID* lid, TransactionID* tid, AccessMode mode)
-    {        
-	return 0;
+    {  
+	int erno = 0;
+	P(mutex);
+	    DBPhysicalID* phid = (DBPhysicalID*) lid->getPhysicalID();
+	    PhysicalIdMap::iterator pos = map_of_locks->find(*phid);
+	    
+	    if (pos == map_of_locks->end())
+	    {
+		/* creating new single lock */
+		SingleLock* lock = new SingleLock(tid, mode, current_sem_key);
+		/* values: 
+		 * 	current_sem    : used for semaphore for reading
+		 * 	current_sem+1  : used for semaphore for writing
+		 */
+		sem_created->insert(current_sem_key);
+		sem_created->insert(current_sem_key+1);
+		current_sem_key += 2;
+		(*map_of_locks)[*phid] = lock;	
+		
+		V(mutex);	
+	    }
+	    else
+	    {
+		/* modifying existing single lock for this PhysicalID */
+		V(mutex);
+		SingleLock* lock = pos->second;
+		//errno = lock->wait_for_lock(tid, mode);
+	    }
+	return erno;
     }
     int LockManager::unlockAll(TransactionID* transaction_id)
     {
-	set<DBPhysicalID*>* locks = (*transaction_locks)[transaction_id];
-	for (set<DBPhysicalID*>::iterator iter = locks->begin();
-	     iter != locks->end(); iter++)
+	P(mutex);
+	TransactionIdMap::iterator pos = transaction_locks->find(*transaction_id);
+	if (pos != transaction_locks->end())
 	{
-	    unlock(*iter, transaction_id);
+	    set<DBPhysicalID>* locks = pos->second;
+	    for (set<DBPhysicalID>::iterator iter = locks->begin();
+	        iter != locks->end(); iter++)
+	    {
+		unlock((*iter), transaction_id);
+	    }
 	}
+	
+	V(mutex);
 	return 0;
     }
-    int LockManager::unlock(DBPhysicalID* pid, TransactionID* tid)
+    int LockManager::unlock(DBPhysicalID phid, TransactionID* tid)
     {
+	/* dziedziczenie sekcji krytycznej */
 	return 0;
-    }
-    LockManager* LockManager::getHandle() { return lockMgr; }
-    
-    LockManager::LockManager() 
-    {
-	transaction_locks = new map<TransactionID* , set<DBPhysicalID*>*>;
-	map_of_locks      = new map<DBPhysicalID*, SingleLock*>;
-	current_sem_key	  = 2000;
     }
 
 
 /*_____SingleLock___________________________________________________*/    
-    SingleLock::SingleLock(int current, AccessMode mode)
+    SingleLock::SingleLock(TransactionID* tid, AccessMode mode, int current_sem)
     {
 	if ( mode == Read)
 	{
@@ -49,8 +131,12 @@ namespace LockMgr{
 	    count_wait_writers = 1;
 	}
 	this->current_mode = mode;
-	wait_readers = create_sem(SEMKEY2);
-	wait_writers = create_sem(SEMKEY3);
+	wait_readers = create_sem(current_sem);
+	wait_writers = create_sem(current_sem+1);
+    }
+    int wait_for_lock(TransactionID * tid, AccessMode mode)
+    {
+	return 0;
     }
     int SingleLock::is_reader_waiting()
     {
--- Lock/Lock.h
+++ Lock/Lock.h
@@ -1,12 +1,13 @@
 #ifndef _LOCK_
 #define _LOCK_
 
-namespace LockMgr { class LockManager; }
+namespace LockMgr { class SingleLock; class LockManager;}
 
 #include "../Store/Store.h"
 #include "../Store/DBPhysicalID.h"
 #include "../TransactionManager/Transaction.h"
 #include "../TransactionManager/Semlib.h"
+#include "../Errors/ErrorConsole.h"
 
 #include <deque>
 #include <map>
@@ -15,66 +16,12 @@ namespace LockMgr { class LockManager; }
 
 using namespace Store;
 using namespace TManager;
+using namespace Errors;
 
 namespace LockMgr {
 
     /* class declaration */
-    class SingleLock;
-    
-    /*
-     *	Singleton Desing Pattern
-     */
-    class LockManager
-    {
-	private:	    
-	    /*
-	     * The only instance of class
-	     */
-	    static LockManager* lockMgr;
-	    
-    	    LockManager();
-	    
-	    /* 
-	     * map from PhysicalId to SingleLock 
-	     */
-	    map<Store::DBPhysicalID* , SingleLock* >* map_of_locks;
-	    
-	    /* 
-	     * map from transactionId to list of PhysicalIds locked 
-	     */
-	    map<TransactionID* , set<DBPhysicalID*>* >* transaction_locks;
-	    
-	    /*
-	     * transaction release object 
-	     * (hovewer it can be locked by other tranasactions if share_lock was used )
-	     */
-	    int unlock(DBPhysicalID*, TransactionID*);
-	    
-	    /* For every DBPhysicalID we need two semaphores: 
-	     * 	a. for readers
-	     * 	b. for writers
-	     * For every semaphore we need a unique number. This number 
-	     * is current_sem_key - incremented with every new semaphore
-	     */
-	    int current_sem_key;
-	    
-	public: 
-	    /*
-	     * Returns the only instance of class
-	     */
-	    static LockManager* getHandle();
-	    
-	    /*
-	     * if object is already locked Transaction will wait on a semaphore
-	     */
-	    int lock(LogicalID* lid, TransactionID* tid, AccessMode mode);
-	    
-	    /*
-	     * Transaction calls commit or rollback on TransactionManager, who calls LockManager
-	     * to unlock all objects related with this transaction
-	     */
-	    int unlockAll(TransactionID* );
-    };
+//    class SingleLock;
 
     /*
      *	Class representing Lock for single Object
@@ -95,12 +42,12 @@ namespace LockMgr {
 	    AccessMode current_mode;
 	    
 	    /* 
-	     * transaction ids that wait for reading an object (wait on a semaphore with unique id = wait_readers)
+	     * transactions that wait for reading an object (wait on a semaphore with unique id = wait_readers)
 	     */
 	    int wait_readers;
 	    
 	    /* 
-	     * transaction ids that wait for writing an object (wait on a semaphore with unique id = wait_writers)
+	     * transactions that wait for writing an object (wait on a semaphore with unique id = wait_writers)
 	     */
 	    int wait_writers;
 	    
@@ -111,11 +58,96 @@ namespace LockMgr {
 	    int count_wait_writers;
 	    
 	public:
-	    SingleLock(int current, AccessMode mode);	
+	    SingleLock(TransactionID* tid, AccessMode mode, int current_sem);	
 	    ~SingleLock();    
 	    int is_reader_waiting();
 	    int is_writer_waiting();	    
 	    void setCurrent(TransactionID* current, AccessMode mode);	    
+    	    int wait_for_lock(TransactionID* tid, AccessMode mode);
     };
+    
+    /* class to compare DBPhysicalID's */    
+    class DBPhysicalIDCmp
+    {
+        public:
+		bool operator() (const DBPhysicalID& , const DBPhysicalID& ) const;
+    }; 
+
+    /* class to compare TransactionID's */
+    class TransactionIDCmp
+    {
+        public:
+		bool operator() (const TransactionID& , const TransactionID& ) const;
+    }; 
+
+
+    /*
+     *	Singleton Desing Pattern
+     */
+    class LockManager
+    {
+	private:	    
+	    /*
+	     * The only instance of class
+	     */
+	    static LockManager* lockMgr;
+	    
+    	    LockManager();
+	    
+	    /* 
+	     * map from PhysicalId to SingleLock, 
+	     */
+	    typedef map<Store::DBPhysicalID , SingleLock* , DBPhysicalIDCmp> PhysicalIdMap;
+	    PhysicalIdMap* map_of_locks;
+	    
+	    /* 
+	     * map from transactionId to list of PhysicalIds locked 
+	     */
+	    typedef map<TransactionID , set<DBPhysicalID>*, TransactionIDCmp> TransactionIdMap;
+	    TransactionIdMap* transaction_locks;
+	    
+	    /*
+	     * transaction released object 
+	     * ( hovewer it can be locked by other tranasactions if share_lock was used )
+	     */
+	    int unlock(DBPhysicalID, TransactionID*);
+	    
+	    /* For every DBPhysicalID we need two semaphores: 
+	     * 	a. for readers
+	     * 	b. for writers
+	     * For every semaphore we need a unique number. This number 
+	     * is current_sem_key - incremented with every new semaphore
+	     */
+	    int current_sem_key;
+	    int mutex;
+	    ErrorConsole err;	    
+	    
+	    /* sem ids that was created - needed to release all sems in destructor */
+	    set<int>* sem_created;
+	    
+	public: 
+	    /*
+	     * Returns the only instance of class
+	     */
+	    static LockManager* getHandle();
+	    
+	    /*
+	     * destruct object - release all semaphores
+	     */
+	     ~LockManager();
+	    /*
+	     * if object is already locked, Transaction tid will wait on a semaphore
+	     */
+	    int lock(LogicalID* , TransactionID* , AccessMode );
+	    
+	    /*
+	     * Transaction calls commit or rollback on TransactionManager, who calls LockManager
+	     * to unlock all objects related with this transaction
+	     */
+	    int unlockAll(TransactionID* );
+    };
+
+
+
 }
 #endif
