77a921d7d1bce596bf89f2f9ff5c782dabe3dda9
pk167277 <pk167277@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2005-12-16 08:18:27 +0000

    Poprawki do zapisywania logow

--- Log/LogIO.cpp
+++ Log/LogIO.cpp
@@ -31,3 +31,114 @@ int LogIO::getFilePos( int fileDes, off_t &result )
   return ( result < 0 ) ? errno : 0;
 }
 
+int LogIO::readDataValue( DataValue *&dv, int fileDes )
+{
+  string s;
+  int errCode;
+
+  if( ( errCode = readString( fileDes, s ) ) ) return errCode;
+
+  // TODO: jak stworzyc DataValue z lancucha?
+
+  return 0;
+}
+
+int LogIO::writeDataValue( DataValue *dv, int fileDes )
+{
+  unsigned char *buffer;
+  int len;
+  int result;
+
+  dv->toByteArray( 0, &len );
+  buffer = new unsigned char[len];
+  dv->toByteArray( &buffer, &len );
+  result = writeString( fileDes, (char *) buffer, len );
+  delete buffer;
+
+  return result;
+}
+
+int LogIO::readTransactionIDVector( vector< TransactionID *> *&tidVec, int fileDes )
+{
+  int errCode;
+  int size;
+
+  if( ( errCode = readInt( fileDes, size ) ) ) return errCode;
+
+  tidVec = new vector< TransactionID *>( size );
+
+  for( int i = 0; i < size; i++ )
+  {
+    TransactionID *tid;
+
+    errCode = readTransactionID( tid, fileDes );
+
+    if( errCode ) return errCode;
+
+    tidVec->operator[](i) = tid;
+  }
+
+  return 0;
+}
+
+int LogIO::writeTransactionIDVector( vector< TransactionID *> *tidVec, int fileDes )
+{
+  int errCode;
+
+  if( ( errCode = writeInt( fileDes, tidVec->size() ) ) ) return errCode;
+  for( unsigned i = 0; i < tidVec->size(); i++ )
+  {
+    errCode = writeTransactionID( tidVec->operator[](i), fileDes );
+    if( errCode ) return errCode;
+  }
+
+  return 0;
+}
+
+int LogIO::readString( int fileDes, string &str )
+{
+  int errCode;
+  unsigned int n = 0;
+  unsigned len;
+  char *buffer;
+
+  if( ( errCode = readInt( fileDes, (int) len ) ) ) return errCode;
+
+  buffer = new char[len];
+
+  while( n < len )
+  {
+    unsigned size = ::read( fileDes, buffer+n, len-n );
+    if( !size ) return UNEXPECTED_EOF_ERROR;
+    n += size;
+    if( errno ) return errno;
+  }
+
+  str.clear();
+  str.append(buffer, len);
+  delete buffer;
+
+  return 0;
+}
+
+int LogIO::writeString( int fileDes, char *buffer, unsigned len )
+{
+  int errCode;
+  unsigned int n = 0;
+
+  if( ( errCode = writeInt( fileDes, len ) ) ) return errCode;
+
+  while( n < len )
+  {
+    unsigned size = ::write( fileDes, buffer+n, len-n );
+    if( !size ) return UNEXPECTED_EOF_ERROR;
+    n += size;
+    if( errno ) return errno;
+  }
+
+  return 0;
+}
+
+
+
+
--- Log/LogIO.h
+++ Log/LogIO.h
@@ -23,18 +23,38 @@ namespace Logs
   {
     public:
     // TODO
-    static int readTransactionID( TransactionID *&tid, int fileDes ) { tid = NULL; return 0; }
-    static int writeTransactionID( TransactionID *tid, int fileDes ) { printf( "Logs: writeTransactionID()\n" ); return 0; }
-    static int readLogicalID( LogicalID *&lid, int fileDes ) { lid = NULL; return 0; }
-    static int writeLogicalID( LogicalID *lid, int fileDes ) { printf( "Logs: writeLogicalID()\n" ); return 0; }
-    static int readDataValue( DataValue *&dv, int fileDes ) { dv = NULL; return 0; }
-    static int writeDataValue( DataValue *dv, int fileDes ) { printf( "Logs: writeDataValue()\n" ); return 0; }
-    static int readTransactionIDVector( vector< TransactionID *> *&tidVec, int fileDes ) { tidVec = NULL; return 0; }
-    static int writeTransactionIDVector( vector< TransactionID *> *tidVec, int fileDes ) { printf( "Logs: writeTransactionIDVector()\n" ); return 0; }
+    static int readTransactionID( TransactionID *&tid, int fileDes )
+    {
+      int id;
+      int errCode;
+
+      if( ( ( errCode = readInt( fileDes, id ) ) ) ) return errCode;
+      tid = new TransactionID( id );
+
+      return 0;
+    }
+    static int writeTransactionID( TransactionID *tid, int fileDes ) { return writeInt( fileDes, tid->getId() ); }
+    static int readLogicalID( LogicalID *&lid, int fileDes )
+    {
+      int id;
+      int errCode;
+
+      if( ( ( errCode = readInt( fileDes, id ) ) ) ) return errCode;
+      // TODO: jak utworzyc LogicalID?
+
+      return 0;
+    }
+    static int writeLogicalID( LogicalID *lid, int fileDes ) { return writeInt( fileDes, lid->toInteger() ); }
+    static int readDataValue( DataValue *&dv, int fileDes );
+    static int writeDataValue( DataValue *dv, int fileDes );
+    static int readTransactionIDVector( vector< TransactionID *> *&tidVec, int fileDes );
+    static int writeTransactionIDVector( vector< TransactionID *> *tidVec, int fileDes );
 
     /// Odczyt/zapis jednej wartosci typu `int`
     static int readInt( int fileDes, int &result );
     static int writeInt( int fileDes, int val );
+    static int readString( int fileDes, string &str );
+    static int writeString( int fileDes, char *buffer, unsigned len );
 
     /// Zwraca wskaznik biezacej pozycji w pliku
     static int getFilePos( int fileDes, off_t &result );
