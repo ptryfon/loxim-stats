82aa4005341c8d42aa0f04b457cf4b422221f1f3
md243003 <md243003@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-12 23:38:38 +0000

    *** empty log message ***

--- Store/PageManager.h
+++ Store/PageManager.h
@@ -2,6 +2,8 @@
 #define __STORE_PAGEMANAGER_H_
 
 #include "Store.h"
+#include "Struct.h"
+#include "PagePointer.h"
 
 namespace Store
 {
@@ -11,6 +13,8 @@ namespace Store
 	public:
 		static int binarizeSize(ObjectPointer *obj);
 		static int binarize(ObjectPointer *obj, unsigned char **buff);
+		static int writeNewHeaders(PagePointer *ptr, page_header hdr, page_header_ex hdx);
+		static int writeObject();
 	};
 
 };
