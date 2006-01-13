c35c9065c85265c69c9a423384f57f8154f9be25
mh209206 <mh209206@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-13 12:45:28 +0000

    uaktualnione TreeNodey o info potrzebne przy statycznej ewaluacji zapytan.

--- QueryParser/QueryParser.cpp
+++ QueryParser/QueryParser.cpp
@@ -3,9 +3,10 @@
 #include <sstream>
 #include <FlexLexer.h>
 #include "TreeNode.h"
+#include "Stack.h"
 #include "QueryParser.h"
-#include "../Errors/ErrorConsole.h"
-#include "../Errors/Errors.h"
+//#include "../Errors/ErrorConsole.h"
+//#include "../Errors/Errors.h"
 
 
 yyFlexLexer* lexer;
@@ -13,28 +14,31 @@ extern QParser::TreeNode *d;
 int yyparse();
 
 // using namespace std;
-using namespace Errors;	
+//using namespace Errors;	
 
 namespace QParser {
 
 	int QueryParser::parseIt(string query, TreeNode *&qTree)
 	{
-		ErrorConsole *ec = new ErrorConsole("Parser");
+//		ErrorConsole *ec = new ErrorConsole("Parser");
 		// ec->init(1);
-		cerr << "stworzyl errConsole \n";
-		*ec << "PARSER::parseIt start\n";
-		cerr << "uzyl errCons\n";
+//		cerr << "stworzyl errConsole \n";
+//		*ec << "PARSER::parseIt start\n";
+//		cerr << "uzyl errCons\n";
 				
 		stringstream ss (stringstream::in | stringstream::out);
 		ss << query;
 		lexer = new yyFlexLexer(&ss); 
 	        int res = yyparse();
 		if (res != 0){
-		    *ec << "zapytanie nie sparsowane robi return...\n";
-		    *ec << (ErrQParser | ENotParsed);	 
-		    return (ErrQParser | ENotParsed);
+//		    *ec << "zapytanie nie sparsowane robi return...\n";
+//		    *ec << (ErrQParser | ENotParsed);	 
+		    cout << "zapytanie nie sparsowane robi return .. \n";
+		    return -1;		    
+//		    return (ErrQParser | ENotParsed);
 		} else
-			    *ec << "zapytanie sparsowane chyba ok\n";
+//			    *ec << "zapytanie sparsowane chyba ok\n";
+		    cout << "zapuytanie sparsowane chyba ok \n";
 		delete lexer;
 		qTree = d;
 		
@@ -44,19 +48,34 @@ namespace QParser {
 		cout << "--------------------------------------" << endl;
 		qTree->putToString();
 		cout << "\n--------------------------------------" << endl;
-		*ec << "PARSER::parseIt end\n";
+//		*ec << "PARSER::parseIt end\n";
+		cout << "koniec parseIt\n";
 
 		return 0;
 	}  
+
 }
-/*
-int main () {
 
+int main (int argc, char* argv[]) {
 
+    string theStr = argv[1];
+    
+    cout << "zaraz zaczne maina \n";
+	cout << "dostalem stringa z parametru: " << theStr <<"\n" ;
     QParser::QueryParser *qp = new QParser::QueryParser();
     QParser::TreeNode *tNode;
-    qp->parseIt("cokolwiek ;", tNode);
-
+    qp->parseIt(theStr, tNode);
+    
+//    cout << "zaraz zrobie factorSub" << endl;
+//    QParser::TreeNode *modNode = tNode->factorSubQuery( tNode->getRArg()->getRArg(), "sik");
+//    cout << "\n ~~~~~~~~~~~~~~~~~~" << endl;
+//    modNode->putToString();
+//    cout << "\n ~~~~~~~~~~~~~~~~~~" << endl;    
+    cout << "skonczylem maina \n";
+    return 0;
 }
-*/
+
+
+
+
 
