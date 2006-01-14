a00dbaa6249a4d12f7f95ed114f7c9aef1213d24
js209298 <js209298@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-14 05:10:36 +0000

    *** empty log message ***

---
+++ QueryParser/DataRead.cpp
@@ -0,0 +1,88 @@
+#include "DataRead.h"
+//#include "../QueryExecutor/QueryResult.h"
+//#include "../QueryExecutor/QueryExecutor.h"
+
+using namespace std;
+//using namespace QExecutor;
+
+namespace QParser {
+
+
+int DataScheme::readData(){
+
+    // ????????????         co zrobic zebym mogl tu wywolac executora i stora?        ??????????
+
+//    QueryExecutor * ecec = new QueryExecutor();		// ??????????????????????????
+
+    DataObjectDef * obj1 = new DataObjectDef();
+    obj1->setMyId(1);
+    obj1->setName("EMP");
+    obj1->setCard("0..*");
+    obj1->setKind("complex");
+    this->addBaseObj(obj1);
+    
+    DataObjectDef * obj10 = new DataObjectDef();
+    obj10->setMyId(10);
+    obj10->setName("NAME");
+    obj10->setCard("1..1");
+    obj10->setKind("atomic");
+    obj10->setType("string");
+    this->addObj(obj10);    
+    
+    DataObjectDef * obj11 = new DataObjectDef();
+    obj11->setMyId(11);
+    obj11->setName("SAL");
+    obj11->setCard("1..1");
+    obj11->setKind("atomic");
+    obj11->setType("int");
+    this->addObj(obj11);
+    
+    DataObjectDef * obj12 = new DataObjectDef();
+    obj12->setMyId(12);
+    obj12->setName("WORKS_IN");
+    obj12->setCard("1..1");
+    obj12->setKind("link");
+    this->addObj(obj12);
+    
+    DataObjectDef * obj30 = new DataObjectDef();
+    obj30->setMyId(30);
+    obj30->setName("DEPT");
+    obj30->setCard("0..*");
+    obj30->setKind("complex");
+    this->addBaseObj(obj30);
+    
+    DataObjectDef * obj37 = new DataObjectDef();
+    obj37->setMyId(37);
+    obj37->setName("DNAME");
+    obj37->setCard("1..1");
+    obj37->setKind("atomic");
+    obj37->setType("string");
+    this->addObj(obj37);
+    
+    DataObjectDef * obj38 = new DataObjectDef();
+    obj38->setMyId(38);
+    obj38->setName("LOC");
+    obj38->setCard("1..*");
+    obj38->setKind("atomic");
+    obj38->setType("string");
+    this->addObj(obj38);
+    
+    
+    obj10->setOwner(getObjById(1));    
+    obj11->setOwner(getObjById(1));
+    obj12->setOwner(getObjById(1));
+    obj12->setTarget(getObjById(30));
+    obj1->addSubObject(getObjById(10));
+    obj1->addSubObject(getObjById(11));
+    obj1->addSubObject(getObjById(12));
+
+    obj37->setOwner(getObjById(30));
+    obj38->setOwner(getObjById(30));
+    obj30->addSubObject(getObjById(37));
+    obj30->addSubObject(getObjById(38));
+
+
+}
+
+
+}
---
+++ QueryParser/DataRead.h
@@ -0,0 +1,175 @@
+#ifndef _DREAD_H_
+#define _DREAD_H_
+
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include "Stack.h"
+
+
+using namespace std;
+using namespace QStack;
+
+namespace QParser {
+
+
+	
+	
+	
+	/*******************************************************
+	 *													   *
+	 *	RAMOWA IMPLEMENTACJA STRUKTURY METABAZY W PAMIECI  *
+	 *													   *
+	 *******************************************************/
+
+	class DataObjectDef
+	{
+	protected:
+		DataObjectDef *nextBase;	/* lista definicji bazowych obiektow */
+/* jsi ??? */	DataObjectDef *nextColl;	/* !=NULL tylko tam, gdzie to elt. jakiejs kolekcji. */	
+		DataObjectDef *nextSub;		/* lista podobjektow jakiegos complex object.*/
+		DataObjectDef *nextHash;	/* lista dla jednego pola tab. haszujacej. */
+		int myId;	/*TODO: czy to jest int? czy unsigned int? czy co? trzeba chyba u executorow sprawdzic*/
+		string name;
+		string card;
+		string kind;
+		bool isBase;	/*czy owner jest NULL*/
+		
+		string type; 				/* TYLKO U ATOMIC */
+		DataObjectDef *subObjects;	/* TYLKO U COMPLEX */
+		//list<DataObjectDef *> subObjects;	/* TYLKO U COMPLEX */
+		DataObjectDef *target;		/* TYLKO U LINK */
+		DataObjectDef *owner;		/* o ile to nie ob. bazowy. wtedy null. */
+		
+	public:
+		virtual ~DataObjectDef(){};
+		virtual DataObjectDef * getSubObjects(){return subObjects;};
+		virtual void setSubObjects(DataObjectDef * sub){subObjects = sub;}
+		virtual void setNextHash(DataObjectDef *p){this->nextHash = p;}
+		virtual DataObjectDef * getNextHash() {return nextHash;}
+		virtual void setNextBase(DataObjectDef *next){nextBase=next;}
+		virtual DataObjectDef * getNextBase(){return nextBase;}
+		virtual int getMyId(){return myId;}
+		virtual void setMyId(int id){myId = id;}
+		virtual string getName(){return name;}
+		virtual void setName(string nname) {name = nname;}
+		virtual string getCard(){return card;}
+		virtual void setCard(string ncard){card = ncard;}
+		
+		virtual void setIsBase(bool b){isBase = b;}
+		
+		virtual bool getIsBase(){return isBase;}
+		
+		virtual string getKind(){return kind;}
+		
+		virtual void setKind(string k){kind = k;}
+		virtual DataObjectDef * getTarget(){return target;}
+		virtual void  setTarget(DataObjectDef * t){target = t;}
+		/* TODO: get/set na wiekszosci tego powyzej... + konstruktory sensowne. */
+		virtual void addSubObject(DataObjectDef * nobj){
+		    nobj->nextSub = this->nextSub;
+		    this->nextSub = nobj;
+		}
+		virtual void setType(string t){type = t;}
+		virtual string getType(){return type;}
+		virtual void setOwner(DataObjectDef * o){owner = o;}
+		virtual DataObjectDef * getOwner(){return owner;} 
+	};
+	
+	class DataScheme 
+	{	
+	protected:
+	DataObjectDef *baseObjects;
+	DataObjectDef *refTable[100]; /*TODO: czy tak sie deklaruje tablice wskaznikow???*/
+	
+	public:
+		int readData();	
+	    	
+		virtual int hashFun (int objId) {return (objId % 100); }
+	
+		virtual void hashIn (int tabPos, DataObjectDef *newObj) {
+			newObj->setNextHash(refTable[tabPos]);	
+			refTable[tabPos] = newObj;			
+		}
+	
+		virtual void addBaseObj (DataObjectDef *newOne) {
+			// DataObjectDef *pom;
+			int newPlace;
+			/*	//	FIXME	nie wiem o co tu chodzilo, czy dodanie obiektu do listy??
+			if (this->baseObjects != NULL) {
+				pom = this->baseObjects->getNextBase();		// czy w ten sposob nie traci sie 
+				this->baseObjects->setNext(NULL);		// pierwszego elemnetu??
+				delete this->baseObjects;
+				newOne->setNext(pom);				
+			}
+			*/			
+			newOne->setNextBase(this->baseObjects);
+			this->baseObjects = newOne;		/*no matter if it was empty before or not. */
+			/* now insert it to the hashtable... */
+			addObj(newOne);
+			// newPlace = this->hashFun(newOne->getMyId());
+			// this->hashIn(newPlace, newOne);
+		}
+		
+		virtual void addObj(DataObjectDef *obj){
+		    int newPlace = this->hashFun(obj->getMyId());
+		    this->hashIn(newPlace, obj);
+		}
+		
+		virtual DataObjectDef *getObjById (int objId) {
+			int itsPlace = this->hashFun (objId);
+			DataObjectDef *point = refTable [itsPlace];
+			bool halt = false;
+			while (not halt) {
+				if (point == NULL) {halt = true;}
+				else if (point->getMyId() == objId) 
+					halt = true;
+				else point = point->getNextHash();		
+			};
+			return point; /*UWAGA to moze byc NULLem (jesli nie znalazl. )*/
+			
+		};
+		
+		
+		virtual list<StatBinder *> statNested (int objId) {
+			DataObjectDef *candidate = this->getObjById(objId);
+			DataObjectDef *pom;
+			list<StatBinder *> bindersCol;
+			if (candidate == NULL)	
+				return bindersCol; //<pusta kolekcja chyba ... >;
+			/*dalej zakladamy ze znalezlismy taki objekt w metabazie... */
+			
+			
+			if (candidate->getKind() == "atomic") return bindersCol;	//<pusta kolekcja>;
+			if (candidate->getKind() == "link") {
+				pom = candidate->getTarget(); /*now pom points to the target object. */
+				StatBinder * sb = new StatBinder (pom->getName(), new SigRef (pom->getMyId()));				
+				bindersCol.push_back(sb);
+				return bindersCol;	//<kolekcja 1-eltowa zawierajaca sb>;				
+			};
+			/*dalej zakladamy ze kind == "complex" -- objekt zlozony. */
+			pom = candidate->getSubObjects();
+				// <kolekcja statBinderow> *binders = new <pusta kolekcja>;	
+			while (pom != NULL) {
+				StatBinder * sb = new StatBinder (pom->getName(), new SigRef (pom->getMyId()));
+				//binders->addBinder(sb);
+				bindersCol.push_back(sb);
+				pom = pom->getSubObjects();
+			}
+			/*
+			for (list <DataObjectDef *>::iterator *it = pom->begin(),
+				*it != pom->end(); it++){
+				StatBinder * sb = new StatBinder (it->getName(), new SigRef (it->getMyId()));
+				//binders->addBinder(sb);
+				bindersCol.push_back(sb);
+			}
+			*/
+			return bindersCol;		
+			//return binders;				
+		};		
+	};	
+}
+
+#endif
