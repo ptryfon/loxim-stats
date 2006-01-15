b8956ec948bcec9d67da6b4dfa4d2e3df95bea8d
jr243017 <jr243017@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-15 23:42:22 +0000

    - dodane kody dla Config - przenumerowane kody z poprawka na errno.h (eliminacja konfliktow)

--- Errors/Errors.h
+++ Errors/Errors.h
@@ -9,52 +9,59 @@
 // return ENOSPC | ErrLogs
 
 namespace Errors {
-#define ErrBackup		 0x1000
-#define	ErrConfig		 0x2000
-#define	ErrDriver		 0x4000
-#define	ErrErrors		 0x8000
-#define	ErrLockMgr		0x10000
-#define	ErrLogs			0x12000
-#define	ErrQExecutor		0x14000
-#define	ErrQParser		0x18000
-#define	ErrSBQLCli		0x20000
-#define	ErrServer		0x22000
-#define	ErrStore		0x24000
-#define	ErrTManager		0x28000
-#define ErrTCPProto		0x40000
-#define ErrAllModules		0xFF000
+#define ErrBackup		 0x100000
+#define	ErrConfig		 0x200000
+#define	ErrDriver		 0x400000
+#define	ErrErrors		 0x800000
+#define	ErrLockMgr		0x1000000
+#define	ErrLogs			0x1200000
+#define	ErrQExecutor		0x1400000
+#define	ErrQParser		0x1800000
+#define	ErrSBQLCli		0x2000000
+#define	ErrServer		0x2200000
+#define	ErrStore		0x2400000
+#define	ErrTManager		0x2800000
+#define ErrTCPProto		0x4000000
+#define ErrAllModules		0xFF00000
 
-#define ENoFile			    0x1
+// error codes 1-255 (0x01 - 0xFF) reserved for system errno.h
+
+#define ENoFile			0x100
 //- Nieobslugiwany/niewlasciwy typ wartosci w CREATE
-#define	EBadType		    0x2
+#define	EBadType		0x200
 //- Blad podczas tworzenia obiektu
-#define	EObjCreate		    0x3
+#define	EObjCreate		0x300
 //- Blad podczas dodawania korzenia
-#define	ERootAdd		    0x4
+#define	ERootAdd		0x400
 //- Nieznany typ wezla drzewa zapytan
-#define	ENoType			    0x5
+#define	ENoType			0x500
 //- Proba pobrania (getResult) QueryResultu z pustego zbioru
-#define EEmptySet		    0x6
+#define EEmptySet		0x600
 // blad pobierania korzenia
-#define EGetRoot		   0x10
+#define EGetRoot		0x700
 
 // Store
-#define EBadFile		    0x7
-#define EPageNotValid		    0x8
-#define EPageNotAquired		    0x9
+#define EBadFile		0x800
+#define EPageNotValid		0x900
+#define EPageNotAquired		0xA00
 
-// Parser
-#define ENotParsed		    0xA
+// Parse error
+#define ENotParsed		0xB00
 
 // Executor
 // - unknown unOp node
-#define EUNodeUNOP		    0xB
+#define EUNodeUNOP		0xC00
 // - unknown algOp node
-#define EUNodeALGOP		    0xC
+#define EUNodeALGOP		0xD00
 // - wrong algOp (arithmetical) argument
-#define EBadALGOPArg		    0xD
+#define EBadALGOPArg		0xE00
 // - wrong deref argument
-#define EBadDerefArg		    0xE
+#define EBadDerefArg		0xF00
+
+// Config
+#define ENotInit		0x1000
+#define ENoValue		0x1100
+#define EBadValue		0x1200
 }
 
 #endif
