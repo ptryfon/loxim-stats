e8ee4df71016c6a7e8aa269f4ab2b8a6538f1a29
js209298 <js209298@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-19 23:14:38 +0000

    *** empty log message ***

---
+++ QueryParser/Optimizer.cpp
@@ -0,0 +1,15 @@
+#include "Optymalizator.h"
+
+
+namespace QParser{
+
+
+
+
+
+
+
+
+
+
+}
---
+++ QueryParser/Optimizer.h
@@ -0,0 +1,17 @@
+#ifndef _OPTYM_H_
+#define _OPTYM_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "TreeNode.h"
+
+
+using namespace std;
+
+namespace QParser{
+
+
+
+}
+#endif
