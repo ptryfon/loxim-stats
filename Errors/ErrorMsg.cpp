e663c7c447c296fa05731313dac3722edbf722fc
baggins <baggins@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-26 20:43:21 +0000

    - komunikaty bledow

---
+++ Errors/ErrorMsg.cpp
@@ -0,0 +1,36 @@
+namespace Errors {
+char *ErrorMsg[] = {
+	"No such file",
+	"Not supported or incorrect type in CREATE",
+	"Error creating object",
+	"Error adding root",
+	"Unknown query tree node type",
+	"Set empty",
+	"Error geting root",
+// Store
+	"Incorrect file",
+	"Page not valid",
+	"Page not aquired",
+// Parse error
+	"Parse error",
+// Executor
+	"Unknown unOp node",
+	"Unknown algOp node",
+	"Wrong algOp (arithmetical) argument",
+	"Wrong deref argument",
+// Config
+	"ENotInit",
+	"ENoValue",
+	"EBadValue",
+// Server
+	"EReceive",
+	"EParse",
+	"EExecute",
+	"ESerialize",
+	"ESend",
+	"EBadResult",
+	"EClientLost",
+// plug for h_errno from gethostbyname(3)
+	"Host not found"
+};
+}
