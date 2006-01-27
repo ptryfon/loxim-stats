b5eefbedcf701696abe76a832921921980f83808
pk167277 <pk167277@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-27 04:09:32 +0000

    dodane wywolanie LogManager::start( storeManager );

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -351,6 +351,7 @@ int Server::Run()
 	storeManager->init(logManager);
 	TransactionManager::getHandle()->init(storeManager, logManager);
 	storeManager->start();
+        logManager->start( storeManager );
 	//storeManager->setTransactionManager(TransactionManager::getHandle());
 	
 	QueryParser *qPa = new QueryParser();
