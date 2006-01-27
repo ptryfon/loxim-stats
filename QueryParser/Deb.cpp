2485ce2f4a328401f8aa86e1490b5137a13e520e
mh209206 <mh209206@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-01-27 16:55:55 +0000

    wrzucone klasy do wypisywania debuggowego.

---
+++ QueryParser/Deb.cpp
@@ -0,0 +1,33 @@
+#include "Deb.h"
+#include "../Config/SBQLConfig.h"
+#include <stdarg.h>
+using namespace Config;
+
+namespace QParser {
+
+    bool Deb::writeDebugs;
+        
+    void Deb::setWriteDebugs() {
+        SBQLConfig cnf("QueryParser");
+        cnf.getBool("debugMode", writeDebugs);
+    }
+
+    bool Deb::ugOn() {
+    	return writeDebugs;
+    }
+    
+    void Deb::ug (const char *fmt, ...) {
+	if (Deb::ugOn()) {
+    	    va_list fmt_args;
+	    fprintf(stderr, "[QParser]: ");
+	    va_start(fmt_args, fmt);
+	    vfprintf(stderr, fmt, fmt_args);
+	    va_end (fmt_args);
+	    fprintf(stderr, "\n");
+	}
+    }
+
+
+}
+
+
---
+++ QueryParser/Deb.h
@@ -0,0 +1,30 @@
+#ifndef _QP_DEBUG_H_
+#define _QP_DEBUG_H_
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include "ClassNames.h"
+
+
+using namespace std;
+
+namespace QParser {
+
+
+    class Deb 
+    {
+    protected:
+	static bool writeDebugs;
+    public:
+	static void ug (const char *fmt, ...);  /* prints out the debug communicate*/
+	static bool ugOn();			/* checks if debugMode is on */
+	static void setWriteDebugs();
+    };
+
+
+}
+
+#endif
+
