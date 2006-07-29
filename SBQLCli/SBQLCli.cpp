900a755bf93e0a2532827d3a4fe68e3ca28f44f6
jk209476 <jk209476@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2006-07-29 09:16:52 +0000

    Zmiany wymuszone przez modul uprawnien. Po pierwsze uzytkownik zanim zacznie prace musi podac login i haslo. Przy pomyslnej weryfikacji moze przystapic do pracy. Nie jest juz mozliwe przekierowanie wejscia klienta tak by czytal zapytania z pliku, dlatego dodano nowe polecenie: @plik_z_zapytaniami Plik zostanie wczytany i wykonane zostana zapytania z pliku. Przykladowo plik moze wygladac: begin / create "ala" as emp / end /

--- SBQLCli/SBQLCli.cpp
+++ SBQLCli/SBQLCli.cpp
@@ -1,6 +1,6 @@
-
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <unistd.h>
 #include "../Driver/DriverManager.h"
@@ -9,7 +9,8 @@
 using namespace std;
 using namespace Driver;
 
-
+void read_and_execute(istream &in, Connection *con );
+void execute_query(Connection *con, string &query);
 
 void printMsg (string str)
 {
@@ -17,6 +18,19 @@ void printMsg (string str)
     cout << str;
 }
 
+void read_login(string &login) {
+    printMsg ("Login: "); 
+    getline(cin, login);    
+}
+
+void read_passwd(string &passwd) {
+    if (isatty(0)) {
+	printMsg ("Password: "); 
+	system("stty -echo");
+	getline(cin, passwd);
+	system("stty echo");    
+    }
+}
 
 int main (int argc, char *argv[])
 {
@@ -41,36 +55,44 @@ int main (int argc, char *argv[])
       exit (1);
     }
 
+  printMsg ("SBQLCli ver 1.0 \n");
+  printMsg ("Ctrl-d exits \n");   
+
+  string login, passwd;
+  read_login(login);
+  read_passwd(passwd);
+  
   /* establishing connection */
   Connection *con;
   try
   {
     //Adam - odkomentuj te jedna linijke:
     //con = DriverManager::getConnection(host, port+1);
-    con = DriverManager::getConnection (host, port);
+    con = DriverManager::getConnection (host, port, login.c_str(), passwd.c_str());
   }
   catch (ConnectionException e)
   {
     cerr << e << endl;
     exit (1);
   }
-
-  printMsg ("SBQLCli ver 1.0 \n");
-  printMsg ("Ctrl-d exits \n");
-
+    
+  printMsg ("\n");
+  
   string line;			// one line of a query
   string input;			// whole query (possibly multi line)
 
-  Result *result    = NULL;
-  Result *resultTmp = NULL;
-  Statement *stmt   = NULL;
-
   printMsg (" > ");
-
   try
   {
     while (getline (cin, line))
       {
+        /* @file_with_queries */
+        if (line[0] == '@') {
+    	    const char *filename = line.c_str() + 1;
+	    ifstream fin(filename);
+	    read_and_execute(fin, con);
+	    fin.close();
+        }
 	if (line[0] != '/')
 	  {
 	    if (!((line.length () >= 2)
@@ -81,35 +103,9 @@ int main (int argc, char *argv[])
 	    printMsg (" \\ ");
 	  }
 	else
-	  {
-	    cerr << "<SBQLCli> Zapytanie: " << input << endl;
-
-	    try
-	    {
-		/*
-		  stmt   = con->parse(input.c_str());
-          stmt->addParam("ans", result);
-          stmt->addParam("empty", NULL);
-          resultTmp = con->execute(stmt);
-          delete stmt;
-          delete result;
-          result = resultTmp;
-        /**/
-
-	      result = con->execute (input.c_str ());
-	    /**/	      
-	      cout << *result << endl;
-
-	    }
-	    catch (ConnectionServerException e)
-	    {			//parse error, or smth.
-	      cerr << e << endl;
-	    }
-
-	    input.clear ();
-	    printMsg (" > ");
-	  }
-
+	{
+	    execute_query(con, input);
+	}
       }	//while
 
   }
@@ -118,11 +114,10 @@ int main (int argc, char *argv[])
     cerr << e << endl;
     exit (1);
   }
-
-
+    
   /* sending abort on exit */
   cerr << endl << "<SBQLCli> auto abort on exit " << endl;
-  result = NULL;
+  Result *result = NULL;
   try
   {
     result = con->execute ("abort;");
@@ -135,4 +130,76 @@ int main (int argc, char *argv[])
 
   delete con;
   return 0;
+}
+
+void read_and_execute(istream &in, Connection *con ) {
+  printMsg ("\n");
+  
+  string line;			// one line of a query
+  string input;			// whole query (possibly multi line)
+
+  printMsg (" > ");
+  try
+  {
+    while (getline (in, line))
+      {
+        /* @file_with_queries */
+        if (line[0] == '@') {
+    	    const char *filename = line.c_str() + 1;
+	    ifstream fin(filename);
+	    read_and_execute(fin, con);
+	    fin.close();
+        }
+	if (line[0] != '/')
+	  {
+	    if (!((line.length () >= 2)
+		  && (line[0] == '-') && (line[1] == '-')))
+	      {
+			input.append (line + " ");
+	      }
+	    printMsg (" \\ ");
+	  }
+	else
+	{
+	    execute_query(con, input);
+	}
+      }	//while
+
+  }
+  catch (ConnectionException e)
+  {
+    cerr << e << endl;
+    exit (1);
+  }
+
+};
+
+void execute_query(Connection *con, string &input) {
+	    cerr << "<SBQLCli> Zapytanie: " << input << endl;
+
+	    try
+	    {
+		/*
+		  stmt   = con->parse(input.c_str());
+          stmt->addParam("ans", result);
+          stmt->addParam("empty", NULL);
+          resultTmp = con->execute(stmt);
+          delete stmt;
+          delete result;
+          result = resultTmp;
+        */
+	     Result *result    = NULL;
+	      result = con->execute (input.c_str ());
+	    /**/	      
+	      cout << *result << endl;
+
+	    }
+	    catch (ConnectionServerException e)
+	    {			//parse error, or smth.
+	      cerr << e << endl;
+	    }
+
+	    input.clear ();
+	    printMsg (" > ");
+
 }
