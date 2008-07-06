798af4ba5fe86a038a7215def6b96719a0023b1e
dk209472 <dk209472@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2008-07-06 19:18:25 +0000

    Dodanie plików brakujący widoku systemowego konfiguracji

---
+++ SystemStats/ConfigStats.cpp
@@ -0,0 +1,82 @@
+#include "ConfigStats.h"
+
+using namespace SystemStatsLib;
+/* =-=-=-=-=-=-=-=-=-=- */
+ConfigOptStats::ConfigOptStats() :
+	SystemStats("OPTION") {
+	setKey("UNKNOWN");
+	setValue("UNKNOWN");
+}
+
+void ConfigOptStats::setKey(string value) {
+	setStringStats("KEY", value);
+}
+
+string ConfigOptStats::getKey() {
+	return getStringStats("KEY");
+}
+
+void ConfigOptStats::setValue(string value) {
+	setStringStats("VALUE", value);
+}
+
+string ConfigOptStats::getValue() {
+	return getStringStats("VALUE");
+}
+
+ConfigOptStats::~ConfigOptStats() {
+}
+
+/* =-=-=-=-=-=-=-=-=-=- */
+
+ConfigModuleStats::ConfigModuleStats() :
+	SystemStats("MODULE") {
+	setModuleName("UNKNOWN");
+}
+
+void ConfigModuleStats::setModuleName(string value) {
+	setStringStats("MODULE_NAME", value);
+}
+
+string ConfigModuleStats::getModuleName() {
+	return getStringStats("MODULE_NAME");
+}
+
+void ConfigModuleStats::addConfigOptStats(string key, ConfigOptStats* value) {
+	setStatsStats(key, value);
+}
+
+ConfigOptStats* ConfigModuleStats::getConfigOptStats(string key) {
+	return dynamic_cast<ConfigOptStats*>(getStatsStats(key));
+
+}
+
+void ConfigModuleStats::removeConfigOptStats(string key) {
+	removeStats(key);
+}
+
+ConfigModuleStats::~ConfigModuleStats() {
+}
+
+/* =-=-=-=-=-=-=-=-=-=- */
+
+ConfigsStats::ConfigsStats() :
+	SystemStats("CONFIGS") {
+
+}
+
+void ConfigsStats::addConfigModuleStats(string key, ConfigModuleStats* value) {
+	setStatsStats(key, value);
+}
+
+ConfigModuleStats* ConfigsStats::getConfigModuleStats(string key) {
+	return dynamic_cast<ConfigModuleStats*>(getStatsStats(key));
+}
+
+void ConfigsStats::removeConfigModuleStats(string key) {
+	removeStats(key);
+}
+
+ConfigsStats::~ConfigsStats() {
+
+}
---
+++ SystemStats/ConfigStats.h
@@ -0,0 +1,57 @@
+/*
+ * ConfigStats.h
+ *
+ *  Created on: 5-lip-08
+ *      Author: damianklata
+ */
+
+#ifndef CONFIGSTATS_H_
+#define CONFIGSTATS_H_
+#include "SystemStats.h"
+
+using namespace std;
+
+namespace SystemStatsLib {
+	class ConfigOptStats: public SystemStats {
+
+	public:
+		ConfigOptStats();
+
+		void setKey(string value);
+		string getKey();
+
+		void setValue(string value);
+		string getValue();
+
+		~ConfigOptStats();
+	};
+
+	class ConfigModuleStats: public SystemStats {
+
+	public:
+		ConfigModuleStats();
+
+		void setModuleName(string value);
+		string getModuleName();
+
+		void addConfigOptStats(string key, ConfigOptStats* value);
+		ConfigOptStats* getConfigOptStats(string key);
+		void removeConfigOptStats(string key);
+
+		~ConfigModuleStats();
+	};
+
+	class ConfigsStats: public SystemStats {
+
+	public:
+		ConfigsStats();
+
+		void addConfigModuleStats(string key, ConfigModuleStats* value);
+		ConfigModuleStats* getConfigModuleStats(string key);
+		void removeConfigModuleStats(string key);
+
+		~ConfigsStats();
+	};
+}
+
+#endif /* CONFIGSTATS_H_ */
