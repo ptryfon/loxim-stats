a543a0f74fd7168b5615cde69cedb79c8eef120d
siersciu <siersciu@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2009-01-20 17:37:26 +0000

    Corrections to protocol regeneration script

--- lw_protogen/run.sh
+++ lw_protogen/run.sh
@@ -4,10 +4,32 @@
 export XERCESC_NLS_HOME=/usr/share/xerces-c/msg
 XML=loxim2.xml
 TARGET=../src/Protocol
+#needs to be ralative!!!
 SKEL=skel
 echo "Cleaning target directory"
-#rm -rf $TARGET
-cp -Rf $SKEL $TARGET
+TO_REMOVE=`find $TARGET | grep -v svn | grep -E "(\.(h|cpp)|Makefile.am)$" `
+echo "About to remove the following files"
+for a in $TO_REMOVE ; do
+	echo rm $a
+done
+echo "Do it? (ctrl-c to abort)"
+read
+for a in $TO_REMOVE ; do
+	rm $a
+done
+TO_COPY=`find $SKEL -type f | grep -v svn | grep -v $SKEL\$`
+SKEL_LEN=${#SKEL}
+echo "About to copy the following files"
+for a in $TO_COPY ; do
+	CUT_NAME=`echo $a| cut -c$(( $SKEL_LEN + 2 ))-`
+	echo cp $a $TARGET/$CUT_NAME
+done
+echo "Do it? (ctrl-c to abort)"
+read
+for a in $TO_COPY ; do
+	CUT_NAME=`echo $a| cut -c$(( $SKEL_LEN + 2 ))-`
+	cp $a $TARGET/$CUT_NAME
+done
 echo "Launching: ./lw_protogen $XML $TARGET"
 if ./lw_protogen $XML $TARGET ; then
 	echo 'Success!'
