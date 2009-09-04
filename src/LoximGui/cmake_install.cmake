924af0e43d09bde59bf63a15e89bb44389c820ae
gtimoszuk <gtimoszuk@5ea2a027-5d39-4ba5-8bbd-4152af6b93f1>
2009-09-04 21:17:38 +0000

    Adddel allInOne library for Protocol LoximGui is building now, beggining of bigger CMakeRefactorization

--- src/LoximGui/cmake_install.cmake
+++ src/LoximGui/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/marek/loxim/src/LoximGui
+# Install script for directory: /home/ballo0/moje/loxim/trunk/src/LoximGui
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,6 +29,6 @@ ENDIF(NOT CMAKE_INSTALL_COMPONENT)
 
 # Install shared libraries without execute permission?
 IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  SET(CMAKE_INSTALL_SO_NO_EXE "0")
+  SET(CMAKE_INSTALL_SO_NO_EXE "1")
 ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
 
